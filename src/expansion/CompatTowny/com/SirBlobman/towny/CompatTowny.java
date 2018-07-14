60fbe1123e96053e221c9a3613fc187e776010ed
SirBlobman <blobmansrealm@gmail.com>
2018-07-14 13:12:18 -0400

    Bleeding Edge - 8.0.0.1
    - Finally fixed a towny bug
    - I have tested this extensively and made sure that the knockback works and that players do not get tagged when they are in a non-pvp town

--- src/expansion/CompatTowny/com/SirBlobman/towny/CompatTowny.java
+++ src/expansion/CompatTowny/com/SirBlobman/towny/CompatTowny.java
@@ -5,16 +5,16 @@ import java.io.File;
 import org.bukkit.Location;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
-import org.bukkit.event.Cancellable;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.player.PlayerTeleportEvent;
 import org.bukkit.util.Vector;
 
 import com.SirBlobman.combatlogx.Combat;
+import com.SirBlobman.combatlogx.config.ConfigLang;
 import com.SirBlobman.combatlogx.config.NoEntryMode;
+import com.SirBlobman.combatlogx.event.PlayerTagEvent;
 import com.SirBlobman.combatlogx.expansion.CLXExpansion;
 import com.SirBlobman.combatlogx.utility.Util;
 import com.SirBlobman.towny.config.ConfigTowny;
@@ -36,72 +36,65 @@ public class CompatTowny implements CLXExpansion, Listener {
 
     public String getUnlocalizedName() {return "CompatTowny";}
     public String getName() {return "Towny Compatibility";}
-    public String getVersion() {return "4";}
-    
+    public String getVersion() {return "5";}
+
     @Override
     public void onConfigReload() {
         ConfigTowny.load();
     }
 
     @EventHandler
-    public void move(PlayerMoveEvent e) {
-        Player p = e.getPlayer();
-        Location from = e.getFrom();
-        Location to = e.getTo();
-        checkEvent(p, e, from, to);
+    public void onAttack(PlayerTagEvent e) {
+        Player tag = e.getPlayer();
+        Location tloc = tag.getLocation();
+        if(!TownyUtil.pvp(tloc)) e.setCancelled(true);
     }
 
     @EventHandler
-    public void tp(PlayerTeleportEvent e) {
+    public void onTeleport(PlayerTeleportEvent e) {
         Player p = e.getPlayer();
-        Location from = e.getFrom();
         Location to = e.getTo();
-        checkEvent(p, e, from, to);
+        if(Combat.isInCombat(p)) {
+            if(!TownyUtil.pvp(to)) {
+                e.setCancelled(true);
+            }
+        }
+
+        String error = ConfigLang.MESSAGE_NO_ENTRY;
+        Util.sendMessage(p, error);
     }
 
-    private void checkEvent(Player p, Cancellable e, Location from, Location to) {
-        if(e.isCancelled()) return;
+    @EventHandler
+    public void onMove(PlayerMoveEvent e) {
+        Player p = e.getPlayer();
+        Location to = e.getTo();
+        Location from = e.getFrom();
+        if(Combat.isInCombat(p)) {
+            if(!TownyUtil.pvp(to)) {
+                if(ConfigTowny.OPTION_NO_SAFEZONE_ENTRY) {
+                    String snem = ConfigTowny.OPTION_NO_SAFEZONE_ENTRY_MODE;
+                    NoEntryMode nem = NoEntryMode.valueOf(snem);
 
-        if (ConfigTowny.OPTION_NO_SAFEZONE_ENTRY && Combat.isInCombat(p)) {
-            boolean pvp = TownyUtil.pvp(to);
-
-            if (!pvp) {
-                if(e instanceof PlayerTeleportEvent) e.setCancelled(true);
-                else if(e instanceof EntityDamageEvent) e.setCancelled(true);
-                else {
-                    if (p.isInsideVehicle()) p.leaveVehicle();
-
-                    NoEntryMode nem = NoEntryMode.valueOf(ConfigTowny.OPTION_NO_SAFEZONE_ENTRY_MODE);
-
-                    if (nem == null) nem = NoEntryMode.CANCEL;
-
-                    if (nem == NoEntryMode.CANCEL) e.setCancelled(true);
-                    else if (nem == NoEntryMode.KILL) p.setHealth(0.0D);
-                    else if (nem == NoEntryMode.TELEPORT) {
+                    if(nem == null || nem == NoEntryMode.CANCEL) p.teleport(from);
+                    else if(nem == NoEntryMode.KILL) p.setHealth(0.0D);
+                    else if(nem == NoEntryMode.TELEPORT) {
                         LivingEntity enemy = Combat.getEnemy(p);
-
-                        if (enemy != null && !enemy.equals(p)) {
-                            Location enemyLoc = enemy.getEyeLocation();
-
-                            p.teleport(enemyLoc);
-                        } else p.teleport(from);
-                    } else if (nem == NoEntryMode.KNOCKBACK) {
-                        Vector fromVector = from.toVector(),
-                                toVector = to.toVector(),
-                                subtracted = fromVector.subtract(toVector);
-
-                        subtracted.normalize();
-                        subtracted.setY(0);
-                        subtracted = subtracted.multiply(ConfigTowny.OPTION_NO_SAFEZONE_ENTRY_STRENGTH);
-
-                        p.setVelocity(subtracted);
+                        if(enemy != null) {
+                            Location loc = enemy.getLocation();
+                            if(loc == null || !TownyUtil.pvp(loc)) p.teleport(from);
+                            else p.teleport(loc);
+                        }
+                    } else if(nem == NoEntryMode.KNOCKBACK) {
+                        Vector vf = from.toVector();
+                        Vector vt = to.toVector();
+                        Vector sub = vf.subtract(vt);
+                        Vector nor = sub.normalize();
+                        Vector v = nor.setY(0.0D);
+                        p.setVelocity(v);
                     }
-                }
-            } else {
-                //Lets check if they're within a town and entered combat
-                if (TownyUtil.getTown(p.getLocation()) != null) {
-                    //They are in a town and attempting combat. Let's cancel it.
-                    e.setCancelled(true);
+
+                    String error = ConfigLang.MESSAGE_NO_ENTRY;
+                    Util.sendMessage(p, error);
                 }
             }
         }
