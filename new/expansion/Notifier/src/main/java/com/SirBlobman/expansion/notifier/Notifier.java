2a84c219fff38aa44a06437b7a0d49f4f6806a29
SirBlobman <blobmansrealm@gmail.com>
2018-10-22 19:28:28 -0400

    1.13 Beta 18
    - Improved FeatherBoard hook (requires config reset)

--- new/expansion/Notifier/src/main/java/com/SirBlobman/expansion/notifier/Notifier.java
+++ new/expansion/Notifier/src/main/java/com/SirBlobman/expansion/notifier/Notifier.java
@@ -1,20 +1,17 @@
 package com.SirBlobman.expansion.notifier;
 
 import java.io.File;
-import java.lang.reflect.Method;
 
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
-import com.SirBlobman.combatlogx.config.Config;
 import com.SirBlobman.combatlogx.event.PlayerCombatTimerChangeEvent;
 import com.SirBlobman.combatlogx.event.PlayerUntagEvent;
 import com.SirBlobman.combatlogx.expansion.CLXExpansion;
 import com.SirBlobman.combatlogx.utility.PluginUtil;
 import com.SirBlobman.combatlogx.utility.SchedulerUtil;
-import com.SirBlobman.combatlogx.utility.Util;
 import com.SirBlobman.expansion.notifier.config.ConfigNotifier;
 import com.SirBlobman.expansion.notifier.utility.ActionBarUtil;
 import com.SirBlobman.expansion.notifier.utility.BossBarUtil;
@@ -24,7 +21,7 @@ import be.maximvdw.featherboard.api.FeatherBoardAPI;
 
 public class Notifier implements CLXExpansion, Listener {
 	public String getUnlocalizedName() {return "Notifier";}
-	public String getVersion() {return "13.1";}
+	public String getVersion() {return "13.2";}
 	
 	public static File FOLDER;
 	
@@ -37,9 +34,9 @@ public class Notifier implements CLXExpansion, Listener {
 	
 	@Override
 	public void disable() {
-		Bukkit.getOnlinePlayers().forEach(p -> {
-			BossBarUtil.removeBossBar(p, true); 
-			ActionBarUtil.removeActionBar(p);
+		Bukkit.getOnlinePlayers().forEach(player -> {
+			BossBarUtil.removeBossBar(player, true); 
+			ActionBarUtil.removeActionBar(player);
 		});
 	}
 	
@@ -50,51 +47,29 @@ public class Notifier implements CLXExpansion, Listener {
 	
 	@EventHandler
 	public void onTimerChange(PlayerCombatTimerChangeEvent e) {
-		Player p = e.getPlayer();
+		Player player = e.getPlayer();
 		
-		if(ConfigNotifier.BOSS_BAR_ENABLED) BossBarUtil.updateBossBar(p);
-		if(ConfigNotifier.ACTION_BAR_ENABLED) ActionBarUtil.updateActionBar(p);
+		if(ConfigNotifier.BOSS_BAR_ENABLED) BossBarUtil.updateBossBar(player);
+		if(ConfigNotifier.ACTION_BAR_ENABLED) ActionBarUtil.updateActionBar(player);
 		if(ConfigNotifier.SCORE_BOARD_ENABLED) {
 			if(ConfigNotifier.SCORE_BOARD_USE_FEATHERBOARD) {
-				try {
-					File folder1 = new File(".").getAbsoluteFile().getParentFile();
-					File folder2 = new File(folder1, "plugins");
-					File folder3 = new File(folder2, "FeatherBoard");
-					File folder4 = new File(folder3, "scoreboards");
-					File file = new File(folder4, "combatlogx.yml");
-					if(!file.exists()) {
-						folder4.mkdirs();
-						Class<Config> clazz = Config.class;
-						Method method = clazz.getDeclaredMethod("copyFromJar", String.class, File.class);
-						method.setAccessible(true);
-						method.invoke(null, "FeatherBoard/scoreboards/combatlogx.yml", folder2);
-						method.setAccessible(false);
-						Bukkit.dispatchCommand(Util.CONSOLE, "fb reload");
-					}
-				} catch(Throwable ex) {
-					String error = "Failed to create default FeatherBoard scoreboard. You will have to create your own.";
-					print(error);
-					ex.printStackTrace();
-				}
-				
-				FeatherBoardAPI.showScoreboard(p, "combatlogx");
-			} else ScoreboardUtil.updateScoreBoard(p);
+				FeatherBoardAPI.showScoreboard(player, ConfigNotifier.SCORE_BOARD_FEATHERBOARD_NAME);
+			} else ScoreboardUtil.updateScoreBoard(player);
 		}
 	}
 	
 	@EventHandler
 	public void onUntag(PlayerUntagEvent e) {
-		Player p = e.getPlayer();
+		Player player = e.getPlayer();
 		
-		if(ConfigNotifier.BOSS_BAR_ENABLED) BossBarUtil.removeBossBar(p, false);
-		if(ConfigNotifier.ACTION_BAR_ENABLED) ActionBarUtil.removeActionBar(p);
+		if(ConfigNotifier.BOSS_BAR_ENABLED) BossBarUtil.removeBossBar(player, false);
+		if(ConfigNotifier.ACTION_BAR_ENABLED) ActionBarUtil.removeActionBar(player);
 		if(ConfigNotifier.SCORE_BOARD_ENABLED) {
 			if(ConfigNotifier.SCORE_BOARD_USE_FEATHERBOARD) {
 				SchedulerUtil.runLater(20L, () -> {
-					FeatherBoardAPI.removeScoreboardOverride(p, "combatlogx"); 
-					FeatherBoardAPI.resetDefaultScoreboard(p);
+					FeatherBoardAPI.removeScoreboardOverride(player, "combatlogx"); 
 				});
-			} else ScoreboardUtil.removeScoreBoard(p);
+			} else ScoreboardUtil.removeScoreBoard(player);
 		}
 	}
 }
--- new/expansion/Notifier/src/main/java/com/SirBlobman/expansion/notifier/config/ConfigNotifier.java
+++ new/expansion/Notifier/src/main/java/com/SirBlobman/expansion/notifier/config/ConfigNotifier.java
@@ -3,11 +3,13 @@ package com.SirBlobman.expansion.notifier.config;
 import java.io.File;
 import java.util.List;
 
+import org.bukkit.Bukkit;
 import org.bukkit.boss.BarColor;
 import org.bukkit.boss.BarStyle;
 import org.bukkit.configuration.file.YamlConfiguration;
 
 import com.SirBlobman.combatlogx.config.Config;
+import com.SirBlobman.combatlogx.utility.PluginUtil;
 import com.SirBlobman.combatlogx.utility.Util;
 import com.SirBlobman.expansion.notifier.Notifier;
 
@@ -21,6 +23,13 @@ public class ConfigNotifier extends Config {
 		if(!file.exists()) copyFromJar("notifier.yml", folder);
 		config = load(file);
 		defaults();
+		
+		if(SCORE_BOARD_USE_FEATHERBOARD && PluginUtil.isEnabled("FeatherBoard")) {
+			File mainFolder = new File(".").getAbsoluteFile().getParentFile();
+			File pluginsFolder = new File(mainFolder, "plugins");
+			copyFromJar("FeatherBoard/scoreboards/combatlogx.yml", pluginsFolder);
+			Bukkit.dispatchCommand(Util.CONSOLE, "fb reload");
+		}
 	}
 
 	public static boolean ACTION_BAR_ENABLED;
@@ -34,9 +43,10 @@ public class ConfigNotifier extends Config {
 	public static String BOSS_BAR_NO_LONGER_IN_COMBAT;
 	
 	public static boolean SCORE_BOARD_ENABLED;
-	public static boolean SCORE_BOARD_USE_FEATHERBOARD;
 	public static String SCORE_BOARD_TITLE;
 	public static List<String> SCORE_BOARD_LINES;
+	public static boolean SCORE_BOARD_USE_FEATHERBOARD;
+	public static String SCORE_BOARD_FEATHERBOARD_NAME;
 	
 	private static void defaults() {
 		ACTION_BAR_ENABLED = get(config, "action bar.enabled", true);
@@ -50,8 +60,9 @@ public class ConfigNotifier extends Config {
 		BOSS_BAR_NO_LONGER_IN_COMBAT = get(config, "boss bar.no longer in combat", "&3Combat &7>> &a&oYou are no longer in combat");
 		
 		SCORE_BOARD_ENABLED = get(config, "score board.enabled", true);
-		SCORE_BOARD_USE_FEATHERBOARD = get(config, "score board.use featherboard", false);
 		SCORE_BOARD_TITLE = get(config, "score board.title", "&6&lCombatLogX");
 		SCORE_BOARD_LINES = get(config, "score board.lines", Util.newList("&f&lTime Left: &7{time_left}", "&f&lEnemy Name: &7{enemy_name}", "&f&lEnemy Health: &7{enemy_health}"));
+		SCORE_BOARD_USE_FEATHERBOARD = get(config, "score board.featherboard.use", false);
+		SCORE_BOARD_FEATHERBOARD_NAME = get(config, "score board.featherboard.board", "combatlogx");
 	}
 }
