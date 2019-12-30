237256a6b8726968cdf741b8e15f7967f6e900f3
SirBlobman <blobmansrealm@gmail.com>
2019-12-30 14:04:39 -0500

    10.0.0.0
    - Added config option for cooldown/warmup/delay to commands after combat time is over
    
    - This completes Feature Request #88

--- expansion/Cheat Prevention/src/main/java/com/SirBlobman/combatlogx/expansion/cheat/prevention/listener/ListenerCommandBlocker.java
+++ expansion/Cheat Prevention/src/main/java/com/SirBlobman/combatlogx/expansion/cheat/prevention/listener/ListenerCommandBlocker.java
@@ -1,8 +1,14 @@
 package com.SirBlobman.combatlogx.expansion.cheat.prevention.listener;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 import com.SirBlobman.combatlogx.api.ICombatLogX;
+import com.SirBlobman.combatlogx.api.event.PlayerUntagEvent;
+import com.SirBlobman.combatlogx.api.event.PlayerUntagEvent.UntagReason;
 import com.SirBlobman.combatlogx.api.expansion.Expansion;
 import com.SirBlobman.combatlogx.api.utility.ICombatManager;
 
@@ -16,16 +22,62 @@ import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 public class ListenerCommandBlocker implements Listener {
     private final Expansion expansion;
     private final ICombatLogX plugin;
+    private final Map<UUID, Long> cooldownMap = new HashMap<>();
     public ListenerCommandBlocker(Expansion expansion) {
         this.expansion = expansion;
         this.plugin = expansion.getPlugin();
+    }
+
+    private boolean isInCooldown(Player player) {
+        if(player == null) return false;
+
+        UUID uuid = player.getUniqueId();
+        long expireTime = cooldownMap.getOrDefault(uuid, 0L);
+        long systemTime = System.currentTimeMillis();
+        if(systemTime >= expireTime) {
+            removeCooldown(player);
+            return false;
+        }
+
+        return true;
+    }
+
+    private void addCooldown(Player player) {
+        if(player == null) return;
+
+        FileConfiguration config = this.expansion.getConfig("cheat-prevention.yml");
+        long cooldownSeconds = config.getLong("command-blocker.delay-after-combat");
+        if(cooldownSeconds <= 0) return;
+
+        long cooldownMillis = TimeUnit.SECONDS.toMillis(cooldownSeconds);
+        long systemTime = System.currentTimeMillis();
+        long expireTime = (systemTime + cooldownMillis);
+
+        UUID uuid = player.getUniqueId();
+        cooldownMap.put(uuid, expireTime);
+    }
+
+    private void removeCooldown(Player player) {
+        if(player == null) return;
+
+        UUID uuid = player.getUniqueId();
+        cooldownMap.remove(uuid);
+    }
+
+    @EventHandler(priority=EventPriority.MONITOR, ignoreCancelled=true)
+    public void onUntag(PlayerUntagEvent e) {
+        UntagReason reason = e.getUntagReason();
+        if(!reason.isExpire()) return;
+
+        Player player = e.getPlayer();
+        addCooldown(player);
     }
 
     @EventHandler(priority=EventPriority.LOWEST, ignoreCancelled=true)
     public void beforeCommand(PlayerCommandPreprocessEvent e) {
         Player player = e.getPlayer();
         ICombatManager combatManager = this.plugin.getCombatManager();
-        if(!combatManager.isInCombat(player)) return;
+        if(!combatManager.isInCombat(player) && !isInCooldown(player)) return;
 
         String command = e.getMessage();
         String actualCommand = convertCommand(command);
