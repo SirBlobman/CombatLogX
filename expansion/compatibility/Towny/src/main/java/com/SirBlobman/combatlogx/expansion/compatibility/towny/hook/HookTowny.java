21061db76acec28bb77c2ab50355b08d9d0c6b9a
SirBlobman <blobmansrealm@gmail.com>
2020-07-04 14:07:24 -0400

    10.0.0.0: Fixed TownyWorld forcePVP causing glitches with safezone detection

--- expansion/compatibility/Towny/src/main/java/com/SirBlobman/combatlogx/expansion/compatibility/towny/hook/HookTowny.java
+++ expansion/compatibility/Towny/src/main/java/com/SirBlobman/combatlogx/expansion/compatibility/towny/hook/HookTowny.java
@@ -6,6 +6,7 @@ import com.palmergames.bukkit.towny.TownyAPI;
 import com.palmergames.bukkit.towny.exceptions.NotRegisteredException;
 import com.palmergames.bukkit.towny.object.Town;
 import com.palmergames.bukkit.towny.object.TownBlock;
+import com.palmergames.bukkit.towny.object.TownyWorld;
 
 public final class HookTowny {
     public static TownyAPI getAPI() {
@@ -17,6 +18,11 @@ public final class HookTowny {
         if(api == null) return null;
 
         return api.getTownBlock(location);
+    }
+    
+    public static TownyWorld getTownWorld(Location location) {
+        TownBlock townBlock = getTownBlock(location);
+        return (townBlock == null ? null : townBlock.getWorld());
     }
 
     public static Town getTown(Location location) {
@@ -31,9 +37,11 @@ public final class HookTowny {
     }
 
     public static boolean isSafeZone(Location location) {
+        TownyWorld townyWorld = getTownWorld(location);
+        if(townyWorld == null) return false;
+        if(townyWorld.isForcePVP()) return false;
+        
         Town town = getTown(location);
-        if(town == null) return false;
-
-        return !town.isPVP();
+        return (town != null && town.isPVP());
     }
 }
