9851fe8b4b6a5ac8f3cf67cea840af41521ff0f8
SirBlobman <blobmansrealm@gmail.com>
2020-07-06 02:14:15 -0400

    10.0.0.0: Updated MVdWPlaceholderAPI to 3.1.0-SNAPSHOT

--- expansion/compatibility/MVdWPlaceholderAPI/src/main/java/com/SirBlobman/combatlogx/expansion/compatibility/mvdwplaceholderapi/hook/HookMVdW.java
+++ expansion/compatibility/MVdWPlaceholderAPI/src/main/java/com/SirBlobman/combatlogx/expansion/compatibility/mvdwplaceholderapi/hook/HookMVdW.java
@@ -1,17 +1,21 @@
 package com.SirBlobman.combatlogx.expansion.compatibility.mvdwplaceholderapi.hook;
 
-import be.maximvdw.placeholderapi.PlaceholderAPI;
-import be.maximvdw.placeholderapi.PlaceholderReplaceEvent;
-import com.SirBlobman.combatlogx.api.ICombatLogX;
-import com.SirBlobman.combatlogx.expansion.compatibility.mvdwplaceholderapi.CompatibilityMVdWPlaceholderAPI;
-import com.SirBlobman.combatlogx.utility.PlaceholderReplacer;
-import org.bukkit.entity.Player;
-import org.bukkit.plugin.java.JavaPlugin;
-
 import java.util.Arrays;
 import java.util.List;
 
-public class HookMVdW implements be.maximvdw.placeholderapi.PlaceholderReplacer {
+import com.SirBlobman.combatlogx.api.ICombatLogX;
+import com.SirBlobman.combatlogx.expansion.compatibility.mvdwplaceholderapi.CompatibilityMVdWPlaceholderAPI;
+
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import be.maximvdw.placeholderapi.PlaceholderAPI;
+import be.maximvdw.placeholderapi.PlaceholderReplaceEvent;
+import be.maximvdw.placeholderapi.PlaceholderReplacer;
+
+import static com.SirBlobman.combatlogx.utility.PlaceholderReplacer.*;
+
+public class HookMVdW implements PlaceholderReplacer {
     private final ICombatLogX plugin;
     public HookMVdW(CompatibilityMVdWPlaceholderAPI expansion) {
         this.plugin = expansion.getPlugin();
@@ -37,15 +41,17 @@ public class HookMVdW implements be.maximvdw.placeholderapi.PlaceholderReplacer 
 
         String id = placeholder.substring("combatlogx_".length());
         switch(id) {
-            case "time_left": return PlaceholderReplacer.getTimeLeftSeconds(this.plugin, player);
-            case "in_combat": return PlaceholderReplacer.getInCombat(this.plugin, player);
-            case "status": return PlaceholderReplacer.getCombatStatus(this.plugin, player);
-            case "enemy_name": return PlaceholderReplacer.getEnemyName(this.plugin, player);
-            case "enemy_health": return PlaceholderReplacer.getEnemyHealth(this.plugin, player);
-            case "enemy_health_rounded": return PlaceholderReplacer.getEnemyHealthRounded(this.plugin, player);
-            case "enemy_hearts": return PlaceholderReplacer.getEnemyHearts(this.plugin, player);
+            case "time_left": return getTimeLeftSeconds(this.plugin, player);
+            case "in_combat": return getInCombat(this.plugin, player);
+            case "status": return getCombatStatus(this.plugin, player);
+            case "enemy_name": return getEnemyName(this.plugin, player);
+            case "enemy_health": return getEnemyHealth(this.plugin, player);
+            case "enemy_health_rounded": return getEnemyHealthRounded(this.plugin, player);
+            case "enemy_hearts": return getEnemyHearts(this.plugin, player);
 
-            default: return null;
+            default: break;
         }
+        
+        return null;
     }
 }
