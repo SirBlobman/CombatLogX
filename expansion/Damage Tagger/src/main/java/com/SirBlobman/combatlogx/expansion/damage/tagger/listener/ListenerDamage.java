55be5284884ad9bddfa3e4e0277dce17e0fbad02
SirBlobman <blobmansrealm@gmail.com>
2020-08-08 08:32:54 -0400

    10.0.0.0: Updated `Damage Tagger` expansion to new language system

--- expansion/Damage Tagger/src/main/java/com/SirBlobman/combatlogx/expansion/damage/tagger/listener/ListenerDamage.java
+++ expansion/Damage Tagger/src/main/java/com/SirBlobman/combatlogx/expansion/damage/tagger/listener/ListenerDamage.java
@@ -1,10 +1,5 @@
 package com.SirBlobman.combatlogx.expansion.damage.tagger.listener;
 
-import com.SirBlobman.combatlogx.api.ICombatLogX;
-import com.SirBlobman.combatlogx.api.event.PlayerPreTagEvent;
-import com.SirBlobman.combatlogx.api.utility.ICombatManager;
-import com.SirBlobman.combatlogx.expansion.damage.tagger.DamageTagger;
-
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
@@ -15,38 +10,21 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 import org.bukkit.projectiles.ProjectileSource;
 
+import com.SirBlobman.combatlogx.api.ICombatLogX;
+import com.SirBlobman.combatlogx.api.event.PlayerPreTagEvent;
+import com.SirBlobman.combatlogx.api.utility.ICombatManager;
+import com.SirBlobman.combatlogx.api.utility.ILanguageManager;
+import com.SirBlobman.combatlogx.expansion.damage.tagger.DamageTagger;
+
 public class ListenerDamage implements Listener {
-    private DamageTagger expansion;
-    private ICombatLogX plugin;
+    private final DamageTagger expansion;
+    private final ICombatLogX plugin;
     public ListenerDamage(DamageTagger expansion) {
         this.expansion = expansion;
         this.plugin = this.expansion.getPlugin();
-    }
-
-    private boolean doesNotTagPlayer(EntityDamageEvent.DamageCause cause) {
-        if(cause == null) return true;
-
-        FileConfiguration config = this.expansion.getConfig("damage-tagger.yml");
-        boolean allDamage = config.getBoolean("all-damage");
-        if(allDamage) return false;
-
-        String causeName = cause.name().toLowerCase().replace("_", "-");
-        String configPath = "damage-type." + causeName;
-        return !config.getBoolean(configPath, false);
-    }
-
-    private String getTagMessage(EntityDamageEvent.DamageCause cause) {
-        if(doesNotTagPlayer(cause)) return null;
-
-        FileConfiguration config = this.expansion.getConfig("damage-tagger.yml");
-        boolean allDamage = config.getBoolean("all-damage");
-        if(allDamage) return this.plugin.getLanguageMessageColoredWithPrefix("damage-tagger.unknown-damage");
-
-        String causeName = cause.name().toLowerCase().replace("_", "-");
-        String messageKey = "damage-tagger.damage-type." + causeName;
-        return this.plugin.getLanguageMessageColoredWithPrefix(messageKey);
     }
 
     @EventHandler(priority=EventPriority.MONITOR, ignoreCancelled=true)
@@ -56,16 +34,17 @@ public class ListenerDamage implements Listener {
         if(checkDamageByEntity(e)) return;
 
         Player player = (Player) entity;
-        EntityDamageEvent.DamageCause cause = e.getCause();
-        if(doesNotTagPlayer(cause)) return;
+        DamageCause damageCause = e.getCause();
+        if(doesNotTagPlayer(damageCause)) return;
 
         ICombatManager combatManager = this.plugin.getCombatManager();
+        ILanguageManager languageManager = this.plugin.getLanguageManager();
         boolean wasInCombat = combatManager.isInCombat(player);
 
         boolean tagged = combatManager.tag(player, null, PlayerPreTagEvent.TagType.UNKNOWN, PlayerPreTagEvent.TagReason.UNKNOWN);
         if(!wasInCombat && tagged) {
-            String message = getTagMessage(cause);
-            this.plugin.sendMessage(player, message);
+            String message = getTagMessage(damageCause);
+            languageManager.sendMessage(player, message);
         }
     }
 
@@ -83,5 +62,30 @@ public class ListenerDamage implements Listener {
         }
 
         return false;
+    }
+
+    private boolean doesNotTagPlayer(EntityDamageEvent.DamageCause cause) {
+        if(cause == null) return true;
+
+        FileConfiguration config = this.expansion.getConfig("damage-tagger.yml");
+        boolean allDamage = config.getBoolean("all-damage");
+        if(allDamage) return false;
+
+        String causeName = cause.name().toLowerCase().replace("_", "-");
+        String configPath = "damage-type." + causeName;
+        return !config.getBoolean(configPath, false);
+    }
+
+    private String getTagMessage(DamageCause damageCause) {
+        if(doesNotTagPlayer(damageCause)) return null;
+        ILanguageManager languageManager = this.plugin.getLanguageManager();
+
+        FileConfiguration config = this.expansion.getConfig("damage-tagger.yml");
+        boolean allDamage = config.getBoolean("all-damage");
+        if(allDamage) return languageManager.getMessageColoredWithPrefix("damage-tagger.unknown-damage");
+
+        String causeName = damageCause.name().toLowerCase().replace("_", "-");
+        String messageKey = "damage-tagger.damage-type." + causeName;
+        return languageManager.getMessageColoredWithPrefix(messageKey);
     }
 }
