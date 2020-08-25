29b6918e718cd5c44925fa059b3e1c59445e96e4
SirBlobman <blobmansrealm@gmail.com>
2020-08-25 19:32:00 -0400

    10.0.0.0: Fix kick ignore list issues and improved command executor.

--- plugin/src/main/java/com/SirBlobman/combatlogx/listener/ListenerUntagger.java
+++ plugin/src/main/java/com/SirBlobman/combatlogx/listener/ListenerUntagger.java
@@ -1,6 +1,8 @@
 package com.SirBlobman.combatlogx.listener;
 
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
@@ -13,25 +15,27 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerKickEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
-import com.SirBlobman.combatlogx.api.ICombatLogX;
+import com.SirBlobman.combatlogx.CombatLogX;
 import com.SirBlobman.combatlogx.api.event.PlayerUntagEvent;
+import com.SirBlobman.combatlogx.api.event.PlayerUntagEvent.UntagReason;
 import com.SirBlobman.combatlogx.api.utility.ICombatManager;
 import com.SirBlobman.combatlogx.api.utility.ILanguageManager;
+import com.SirBlobman.combatlogx.manager.CombatManager;
 
 public class ListenerUntagger implements Listener {
-    private final ICombatLogX plugin;
-    public ListenerUntagger(ICombatLogX plugin) {
+    private final CombatLogX plugin;
+    public ListenerUntagger(CombatLogX plugin) {
         this.plugin = plugin;
     }
 
     @EventHandler(priority=EventPriority.LOWEST, ignoreCancelled=true)
     public void onKicked(PlayerKickEvent e) {
         String kickReason = e.getReason();
-        if(isKickReasonIgnored(kickReason)) return;
+        UntagReason untagReason = (isKickReasonIgnored(kickReason) ? UntagReason.EXPIRE : UntagReason.KICK);
         
         Player player = e.getPlayer();
         ICombatManager combatManager = this.plugin.getCombatManager();
-        combatManager.untag(player, PlayerUntagEvent.UntagReason.KICK);
+        combatManager.untag(player, untagReason);
     }
 
     @EventHandler(priority=EventPriority.LOWEST, ignoreCancelled=true)
@@ -67,34 +71,61 @@ public class ListenerUntagger implements Listener {
 
     private void runUntagCommands(Player player) {
         if(player == null) return;
-
-        ICombatManager combatManager = this.plugin.getCombatManager();
+        CombatManager combatManager = this.plugin.getCombatManager();
         FileConfiguration config = this.plugin.getConfig("config.yml");
+
         List<String> sudoCommandList = config.getStringList("untag-sudo-command-list");
         for(String sudoCommand : sudoCommandList) {
             sudoCommand = combatManager.getSudoCommand(player, null, sudoCommand);
             if(sudoCommand.startsWith("[PLAYER]")) {
                 String command = sudoCommand.substring("[PLAYER]".length());
-                player.performCommand(command);
+                runAsPlayer(player, command);
                 continue;
             }
 
             if(sudoCommand.startsWith("[OP]")) {
                 String command = sudoCommand.substring("[OP]".length());
-                if(player.isOp()) {
-                    player.performCommand(command);
-                    continue;
-                }
-
-                player.setOp(true);
-                player.performCommand(command);
-                player.setOp(false);
-
+                runAsOp(player, command);
                 continue;
             }
 
+            runAsConsole(sudoCommand);
+        }
+    }
+
+    private void runAsConsole(String command) {
+        try {
             CommandSender console = Bukkit.getConsoleSender();
-            Bukkit.dispatchCommand(console, sudoCommand);
+            Bukkit.dispatchCommand(console, command);
+        } catch(Exception ex) {
+            Logger logger = this.plugin.getLogger();
+            logger.log(Level.WARNING, "Failed to execute console command '" + command + "':", ex);
+        }
+    }
+
+    private void runAsPlayer(Player player, String command) {
+        try {
+            player.performCommand(command);
+        } catch(Exception ex) {
+            Logger logger = this.plugin.getLogger();
+            logger.log(Level.WARNING, "Failed to execute command '" + command + "' as a player:", ex);
+        }
+    }
+
+    private void runAsOp(Player player, String command) {
+        if(player.isOp()) {
+            runAsPlayer(player, command);
+            return;
+        }
+
+        try {
+            player.setOp(true);
+            player.performCommand(command);
+        } catch(Exception ex) {
+            Logger logger = this.plugin.getLogger();
+            logger.log(Level.WARNING, "Failed to execute command '" + command + "' as OP:", ex);
+        } finally {
+            player.setOp(false);
         }
     }
     
