35dc698c1bc54ec3968e6b0a12b244521612fca7
SirBlobman <blobmansrealm@gmail.com>
2021-02-27 02:20:36 -0500

    11.0.0.0: Begin working on loot protection expansion.

---
+++ expansion/loot-protection/src/main/java/combatlogx/expansion/loot/protection/LootProtectionExpansion.java
@@ -0,0 +1,30 @@
+package combatlogx.expansion.loot.protection;
+
+import com.github.sirblobman.combatlogx.api.ICombatLogX;
+import com.github.sirblobman.combatlogx.api.expansion.Expansion;
+
+public final class LootProtectionExpansion extends Expansion {
+    public LootProtectionExpansion(ICombatLogX plugin) {
+        super(plugin);
+    }
+
+    @Override
+    public void onLoad() {
+        getLogger().info("This expansion is still WIP!");
+    }
+
+    @Override
+    public void onEnable() {
+        getLogger().info("This expansion is still WIP!");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("This expansion is still WIP!");
+    }
+
+    @Override
+    public void reloadConfig() {
+        getLogger().info("This expansion is still WIP!");
+    }
+}
