5fe129f00e6e9a312be85b2d841adaa1b42332f6
SirBlobman <blobmansrealm@gmail.com>
2021-03-04 00:04:21 -0500

    11.0.0.0: Finished GriefPrevention compatibility expansion.

---
+++ expansion/compatibility/GriefPrevention/src/main/java/combatlogx/expansion/compatibility/region/grief/prevention/GriefPreventionExpansion.java
@@ -0,0 +1,27 @@
+package combatlogx.expansion.compatibility.region.grief.prevention;
+
+import com.github.sirblobman.combatlogx.api.ICombatLogX;
+import com.github.sirblobman.combatlogx.api.expansion.region.RegionExpansion;
+import com.github.sirblobman.combatlogx.api.expansion.region.RegionHandler;
+
+public final class GriefPreventionExpansion extends RegionExpansion {
+    private RegionHandler regionHandler;
+    public GriefPreventionExpansion(ICombatLogX plugin) {
+        super(plugin);
+        this.regionHandler = null;
+    }
+
+    @Override
+    public boolean checkDependencies() {
+        return checkDependency("GriefPrevention", true);
+    }
+
+    @Override
+    public RegionHandler getRegionHandler() {
+        if(this.regionHandler == null) {
+            this.regionHandler = new GriefPreventionRegionHandler(this);
+        }
+
+        return this.regionHandler;
+    }
+}
---
+++ expansion/compatibility/GriefPrevention/src/main/java/combatlogx/expansion/compatibility/region/grief/prevention/GriefPreventionRegionHandler.java
@@ -0,0 +1,32 @@
+package combatlogx.expansion.compatibility.region.grief.prevention;
+
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import com.github.sirblobman.combatlogx.api.expansion.region.RegionHandler;
+import com.github.sirblobman.combatlogx.api.object.TagType;
+
+import me.ryanhamshire.GriefPrevention.Claim;
+import me.ryanhamshire.GriefPrevention.GriefPrevention;
+
+@SuppressWarnings("UnstableApiUsage")
+public final class GriefPreventionRegionHandler extends RegionHandler {
+    public GriefPreventionRegionHandler(GriefPreventionExpansion expansion) {
+        super(expansion);
+    }
+
+    @Override
+    public String getEntryDeniedMessagePath(TagType tagType) {
+        return "expansion.griefprevention-compatibility-no-entry";
+    }
+
+    @Override
+    public boolean isSafeZone(Player player, Location location, TagType tagType) {
+        if(tagType == TagType.UNKNOWN) return false;
+
+        GriefPrevention griefPrevention = JavaPlugin.getPlugin(GriefPrevention.class);
+        Claim claim = griefPrevention.dataStore.getClaimAt(location, false, null);
+        return griefPrevention.claimIsPvPSafeZone(claim);
+    }
+}
