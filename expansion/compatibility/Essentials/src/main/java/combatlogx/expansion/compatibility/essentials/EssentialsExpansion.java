6c12ba88f7ee1cffad1b1f135f135c5bfcba85a0
SirBlobman <blobmansrealm@gmail.com>
2021-03-06 15:20:06 -0500

    11.0.0.0: Created EssentialsX compatibility expansion (Vanish and TP Requests)

---
+++ expansion/compatibility/Essentials/src/main/java/combatlogx/expansion/compatibility/essentials/EssentialsExpansion.java
@@ -0,0 +1,43 @@
+package combatlogx.expansion.compatibility.essentials;
+
+import com.github.sirblobman.combatlogx.api.ICombatLogX;
+import com.github.sirblobman.combatlogx.api.expansion.Expansion;
+import com.github.sirblobman.combatlogx.api.expansion.ExpansionConfigurationManager;
+import com.github.sirblobman.combatlogx.api.expansion.ExpansionManager;
+
+import combatlogx.expansion.compatibility.essentials.listener.ListenerEssentials;
+
+public final class EssentialsExpansion extends Expansion {
+    public EssentialsExpansion(ICombatLogX plugin) {
+        super(plugin);
+    }
+
+    @Override
+    public void onLoad() {
+        ExpansionConfigurationManager configurationManager = getConfigurationManager();
+        configurationManager.saveDefault("config.yml");
+    }
+
+    @Override
+    public void onEnable() {
+        if(!checkDependency("Essentials", true)) {
+            ICombatLogX plugin = getPlugin();
+            ExpansionManager expansionManager = plugin.getExpansionManager();
+            expansionManager.disableExpansion(this);
+            return;
+        }
+
+        new ListenerEssentials(this).register();
+    }
+
+    @Override
+    public void onDisable() {
+        // Do Nothing
+    }
+
+    @Override
+    public void reloadConfig() {
+        ExpansionConfigurationManager configurationManager = getConfigurationManager();
+        configurationManager.reload("config.yml");
+    }
+}
---
+++ expansion/compatibility/Essentials/src/main/java/combatlogx/expansion/compatibility/essentials/listener/ListenerEssentials.java
@@ -0,0 +1,101 @@
+package combatlogx.expansion.compatibility.essentials.listener;
+
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import com.github.sirblobman.api.language.LanguageManager;
+import com.github.sirblobman.combatlogx.api.ICombatLogX;
+import com.github.sirblobman.combatlogx.api.ICombatManager;
+import com.github.sirblobman.combatlogx.api.event.PlayerPreTagEvent;
+import com.github.sirblobman.combatlogx.api.expansion.Expansion;
+import com.github.sirblobman.combatlogx.api.expansion.ExpansionConfigurationManager;
+import com.github.sirblobman.combatlogx.api.expansion.ExpansionListener;
+
+import com.earth2me.essentials.CommandSource;
+import com.earth2me.essentials.Essentials;
+import com.earth2me.essentials.User;
+import combatlogx.expansion.compatibility.essentials.EssentialsExpansion;
+import net.ess3.api.IUser;
+import net.ess3.api.events.TPARequestEvent;
+
+public final class ListenerEssentials extends ExpansionListener {
+    public ListenerEssentials(EssentialsExpansion expansion) {
+        super(expansion);
+    }
+
+    @EventHandler(priority=EventPriority.NORMAL, ignoreCancelled=true)
+    public void onTeleportRequest(TPARequestEvent e) {
+        if(isTeleportRequestEnabled()) return;
+
+        ICombatLogX plugin = getCombatLogX();
+        ICombatManager combatManager = plugin.getCombatManager();
+        LanguageManager languageManager = getLanguageManager();
+
+        CommandSource requester = e.getRequester();
+        Player player = requester.getPlayer();
+        if(player == null) return;
+
+        if(combatManager.isInCombat(player)) {
+            languageManager.sendMessage(player, "expansion.essentials-compatibility.prevent-teleport-request-self", null, true);
+            e.setCancelled(true);
+            return;
+        }
+
+        IUser targetUser = e.getTarget();
+        Player target = targetUser.getBase();
+        if(target == null) return;
+
+        if(combatManager.isInCombat(target)) {
+            languageManager.sendMessage(player, "expansion.essentials-compatibility.prevent-teleport-request-other", null, true);
+            e.setCancelled(true);
+            // return;
+        }
+    }
+
+    @EventHandler(priority=EventPriority.NORMAL, ignoreCancelled=true)
+    public void beforeCombat(PlayerPreTagEvent e) {
+        Player player = e.getPlayer();
+        if(isVanished(player) && preventVanishSelfTag()) {
+            e.setCancelled(true);
+        }
+
+        LivingEntity enemy = e.getEnemy();
+        if(enemy instanceof Player) {
+            Player other = (Player) enemy;
+            if(isVanished(other) && preventVanishOtherTag()) {
+                e.setCancelled(true);
+            }
+        }
+    }
+
+    private YamlConfiguration getConfiguration() {
+        Expansion expansion = getExpansion();
+        ExpansionConfigurationManager configurationManager = expansion.getConfigurationManager();
+        return configurationManager.get("config.yml");
+    }
+
+    private boolean isTeleportRequestEnabled() {
+        YamlConfiguration configuration = getConfiguration();
+        return !configuration.getBoolean("prevent-teleport-request");
+    }
+
+    private boolean preventVanishSelfTag() {
+        YamlConfiguration configuration = getConfiguration();
+        return configuration.getBoolean("prevent-vanish-tagging-self");
+    }
+
+    private boolean preventVanishOtherTag() {
+        YamlConfiguration configuration = getConfiguration();
+        return configuration.getBoolean("prevent-vanish-tagging-other");
+    }
+
+    private boolean isVanished(Player player) {
+        Essentials plugin = JavaPlugin.getPlugin(Essentials.class);
+        User user = plugin.getUser(player);
+        return user.isVanished();
+    }
+}
