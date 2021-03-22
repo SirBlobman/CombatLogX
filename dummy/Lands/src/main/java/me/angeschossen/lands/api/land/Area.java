995ed2c4e9d741e78a52f2fa06749167e740fa63
SirBlobman <blobmansrealm@gmail.com>
2021-03-22 18:39:04 -0400

    11.0.0.0: Update LandsAPI to 5.3.2

--- dummy/Lands/src/main/java/me/angeschossen/lands/api/land/Area.java
+++
@@ -1,9 +0,0 @@
-package me.angeschossen.lands.api.land;
-
-import org.bukkit.entity.Player;
-
-import me.angeschossen.lands.api.role.enums.RoleSetting;
-
-public interface Area {
-    boolean canSetting(Player player, RoleSetting action, boolean sendMessage);
-}
--- dummy/Lands/src/main/java/me/angeschossen/lands/api/role/enums/RoleSetting.java
+++
@@ -1,5 +0,0 @@
-package me.angeschossen.lands.api.role.enums;
-
-public enum RoleSetting {
-    ATTACK_PLAYER, ATTACK_ANIMAL;
-}
