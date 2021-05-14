5c96519096d5b6cdd5c5afef260f238a4b56410b
SirBlobman <blobmansrealm@gmail.com>
2021-05-14 06:44:40 -0400

    11.0.0.0: Finished VanishNoPacket expansion.

---
+++ expansion/compatibility/VanishNoPacket/src/main/java/combatlogx/expansion/compatibility/vnp/VanishNoPacketExpansion.java
@@ -0,0 +1,43 @@
+package combatlogx.expansion.compatibility.vnp;
+
+import com.github.sirblobman.api.configuration.ConfigurationManager;
+import com.github.sirblobman.combatlogx.api.ICombatLogX;
+import com.github.sirblobman.combatlogx.api.expansion.Expansion;
+import com.github.sirblobman.combatlogx.api.expansion.ExpansionManager;
+
+import combatlogx.expansion.compatibility.vnp.listener.ListenerVanish;
+
+public final class VanishNoPacketExpansion extends Expansion {
+    public VanishNoPacketExpansion(ICombatLogX plugin) {
+        super(plugin);
+    }
+
+    @Override
+    public void onLoad() {
+        ConfigurationManager configurationManager = getConfigurationManager();
+        configurationManager.saveDefault("config.yml");
+    }
+
+    @Override
+    public void onEnable() {
+        if(!checkDependency("VanishNoPacket", true, "3")) {
+            ICombatLogX plugin = getPlugin();
+            ExpansionManager expansionManager = plugin.getExpansionManager();
+            expansionManager.disableExpansion(this);
+            return;
+        }
+
+        new ListenerVanish(this).register();
+    }
+
+    @Override
+    public void onDisable() {
+        // Do Nothing
+    }
+
+    @Override
+    public void reloadConfig() {
+        ConfigurationManager configurationManager = getConfigurationManager();
+        configurationManager.reload("config.yml");
+    }
+}
---
+++ expansion/compatibility/VanishNoPacket/src/main/java/combatlogx/expansion/compatibility/vnp/listener/ListenerVanish.java
@@ -0,0 +1,66 @@
+package combatlogx.expansion.compatibility.vnp.listener;
+
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import com.github.sirblobman.api.configuration.ConfigurationManager;
+import com.github.sirblobman.combatlogx.api.event.PlayerPreTagEvent;
+import com.github.sirblobman.combatlogx.api.expansion.Expansion;
+import com.github.sirblobman.combatlogx.api.expansion.ExpansionListener;
+
+import combatlogx.expansion.compatibility.vnp.VanishNoPacketExpansion;
+import org.kitteh.vanish.VanishManager;
+import org.kitteh.vanish.VanishPlugin;
+
+public final class ListenerVanish extends ExpansionListener {
+    public ListenerVanish(VanishNoPacketExpansion expansion) {
+        super(expansion);
+    }
+
+    @EventHandler(priority=EventPriority.NORMAL, ignoreCancelled=true)
+    public void beforeCombat(PlayerPreTagEvent e) {
+        Player player = e.getPlayer();
+        if(isVanished(player) && preventVanishSelfTag()) {
+            e.setCancelled(true);
+        }
+
+        LivingEntity enemy = e.getEnemy();
+        if(enemy instanceof Player) {
+            Player other = (Player) enemy;
+            if(isVanished(other) && preventVanishOtherTag()) {
+                e.setCancelled(true);
+            }
+        }
+    }
+
+    private YamlConfiguration getConfiguration() {
+        Expansion expansion = getExpansion();
+        ConfigurationManager configurationManager = expansion.getConfigurationManager();
+        return configurationManager.get("config.yml");
+    }
+
+    private boolean isTeleportRequestEnabled() {
+        YamlConfiguration configuration = getConfiguration();
+        return !configuration.getBoolean("prevent-teleport-request");
+    }
+
+    private boolean preventVanishSelfTag() {
+        YamlConfiguration configuration = getConfiguration();
+        return configuration.getBoolean("prevent-vanish-tagging-self");
+    }
+
+    private boolean preventVanishOtherTag() {
+        YamlConfiguration configuration = getConfiguration();
+        return configuration.getBoolean("prevent-vanish-tagging-other");
+    }
+
+    private boolean isVanished(Player player) {
+        VanishPlugin vanishPlugin = JavaPlugin.getPlugin(VanishPlugin.class);
+        VanishManager vanishManager = vanishPlugin.getManager();
+        return vanishManager.isVanished(player);
+    }
+}
