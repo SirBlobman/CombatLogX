1da085b0abdc8ab01566acc31b2026cef32ec8de
SirBlobman <blobmansrealm@gmail.com>
2021-05-14 08:39:42 -0400

    11.0.0.0: Updated KingdomsX expansion to KingdomsX v1.10.18.1

--- expansion/compatibility/KingdomsX/src/main/java/combatlogx/expansion/compatibility/region/kingdomsx/KingdomsXRegionHandler.java
+++ expansion/compatibility/KingdomsX/src/main/java/combatlogx/expansion/compatibility/region/kingdomsx/KingdomsXRegionHandler.java
@@ -1,17 +1,12 @@
 package combatlogx.expansion.compatibility.region.kingdomsx;
 
-import java.util.Set;
-import java.util.UUID;
-
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
 import com.github.sirblobman.combatlogx.api.expansion.region.RegionHandler;
 import com.github.sirblobman.combatlogx.api.object.TagType;
 
-import org.kingdoms.constants.kingdom.Kingdom;
 import org.kingdoms.constants.land.Land;
-import org.kingdoms.constants.player.KingdomPlayer;
 
 public final class KingdomsXRegionHandler extends RegionHandler {
     public KingdomsXRegionHandler(KingdomsXExpansion expansion) {
@@ -26,21 +21,8 @@ public final class KingdomsXRegionHandler extends RegionHandler {
     @Override
     public boolean isSafeZone(Player player, Location location, TagType tagType) {
         if(tagType != TagType.PLAYER) return false;
-        UUID playerId = player.getUniqueId();
 
         Land land = Land.getLand(location);
-        if(land == null || land.isBeingInvaded() || !land.isClaimed()) return false;
-
-        UUID claimedBy = land.getClaimedBy();
-        if(claimedBy == null || !claimedBy.equals(playerId)) return false;
-
-        KingdomPlayer claimer = land.getClaimer();
-        if(claimer == null) return false;
-
-        Kingdom kingdom = claimer.getKingdom();
-        if(kingdom == null) return false;
-
-        Set<UUID> kingdomMemberSet = kingdom.getMembers();
-        return !kingdomMemberSet.contains(playerId);
+        return (land != null && land.isClaimed() && !land.isBeingInvaded());
     }
 }
