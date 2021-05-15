4bdfb4af7cfd22ed1d96717a20033fc3e581a6b7
Olivo <olivoz12345@gmail.com>
2021-05-15 12:32:39 +0200

    Remove unused imports

--- expansion/compatibility/ProtectionStones/src/main/java/combatlogx/expansion/compatibility/region/protectionstones/ProtectionStonesExpansion.java
+++ expansion/compatibility/ProtectionStones/src/main/java/combatlogx/expansion/compatibility/region/protectionstones/ProtectionStonesExpansion.java
@@ -1,12 +1,9 @@
 package combatlogx.expansion.compatibility.region.protectionstones;
 
 import com.github.sirblobman.combatlogx.api.ICombatLogX;
-import com.github.sirblobman.combatlogx.api.expansion.Expansion;
 import com.github.sirblobman.combatlogx.api.expansion.region.RegionExpansion;
 import com.github.sirblobman.combatlogx.api.expansion.region.RegionHandler;
 import combatlogx.expansion.compatibility.region.protectionstones.handler.ProtectionStonesRegionHandler;
-
-import java.util.logging.Level;
 
 public final class ProtectionStonesExpansion extends RegionExpansion {
     private RegionHandler regionHandler = null;
