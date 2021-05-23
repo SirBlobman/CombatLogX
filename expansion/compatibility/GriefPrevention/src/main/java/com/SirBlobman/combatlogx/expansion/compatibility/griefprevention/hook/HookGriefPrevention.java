c9517e414eb7caf81f015e02e1c39c18e9848d34
SirBlobman <blobmansrealm@gmail.com>
2021-05-23 13:46:43 -0400

    Small change to GriefPrevention expansion

--- expansion/compatibility/GriefPrevention/src/main/java/com/SirBlobman/combatlogx/expansion/compatibility/griefprevention/hook/HookGriefPrevention.java
+++ expansion/compatibility/GriefPrevention/src/main/java/com/SirBlobman/combatlogx/expansion/compatibility/griefprevention/hook/HookGriefPrevention.java
@@ -7,14 +7,9 @@ import me.ryanhamshire.GriefPrevention.Claim;
 import me.ryanhamshire.GriefPrevention.GriefPrevention;
 
 public final class HookGriefPrevention {
-    public static GriefPrevention getAPI() {
-        return JavaPlugin.getPlugin(GriefPrevention.class);
-    }
-
     public static boolean isSafeZone(Location location) {
-        GriefPrevention api = getAPI();
-        Claim claim = api.dataStore.getClaimAt(location, false, null);
-
+        GriefPrevention griefPrevention = JavaPlugin.getPlugin(GriefPrevention.class);
+        Claim claim = griefPrevention.dataStore.getClaimAt(location, false, null);
         return (claim != null);
     }
 }
