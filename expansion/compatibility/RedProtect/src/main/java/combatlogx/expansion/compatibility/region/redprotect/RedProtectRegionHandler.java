11423a1abd8a9d5c767cf08ef997b463443f6f3a
SirBlobman <blobmansrealm@gmail.com>
2021-05-31 01:17:07 -0400

    11.0.0.0: Created RedProtect compatibility expansion.

---
+++ expansion/compatibility/RedProtect/src/main/java/combatlogx/expansion/compatibility/region/redprotect/RedProtectRegionHandler.java
@@ -0,0 +1,33 @@
+package combatlogx.expansion.compatibility.region.redprotect;
+
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+
+import com.github.sirblobman.combatlogx.api.expansion.region.RegionHandler;
+import com.github.sirblobman.combatlogx.api.object.TagType;
+
+import br.net.fabiozumbi12.RedProtect.Bukkit.API.RedProtectAPI;
+import br.net.fabiozumbi12.RedProtect.Bukkit.RedProtect;
+import br.net.fabiozumbi12.RedProtect.Bukkit.Region;
+
+public class RedProtectRegionHandler extends RegionHandler {
+    public RedProtectRegionHandler(RedProtectExpansion expansion) {
+        super(expansion);
+    }
+
+    @Override
+    public String getEntryDeniedMessagePath(TagType tagType) {
+        return "expansion.redprotect-compatibility-no-entry";
+    }
+
+    @Override
+    public boolean isSafeZone(Player player, Location location, TagType tagType) {
+        if(tagType != TagType.PLAYER) return false;
+
+        RedProtect redProtect = RedProtect.get();
+        RedProtectAPI redProtectApi = redProtect.getAPI();
+
+        Region region = redProtectApi.getRegion(location);
+        return (region != null && !region.getFlagBool("pvp"));
+    }
+}
