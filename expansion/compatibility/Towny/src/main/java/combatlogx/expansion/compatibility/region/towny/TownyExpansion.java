b18cb7e4955317fa9556bd4ef45701f4876f484d
SirBlobman <blobmansrealm@gmail.com>
2021-05-31 02:06:18 -0400

    11.0.0.0: Complete Towny compatibility expansion.
    Closes #187

---
+++ expansion/compatibility/Towny/src/main/java/combatlogx/expansion/compatibility/region/towny/TownyExpansion.java
@@ -0,0 +1,27 @@
+package combatlogx.expansion.compatibility.region.towny;
+
+import com.github.sirblobman.combatlogx.api.ICombatLogX;
+import com.github.sirblobman.combatlogx.api.expansion.region.RegionExpansion;
+import com.github.sirblobman.combatlogx.api.expansion.region.RegionHandler;
+
+public final class TownyExpansion extends RegionExpansion {
+    private RegionHandler regionHandler;
+    public TownyExpansion(ICombatLogX plugin) {
+        super(plugin);
+        this.regionHandler = null;
+    }
+
+    @Override
+    public boolean checkDependencies() {
+        return checkDependency("Towny", true, "0.97");
+    }
+
+    @Override
+    public RegionHandler getRegionHandler() {
+        if(this.regionHandler == null) {
+            this.regionHandler = new TownyRegionHandler(this);
+        }
+
+        return this.regionHandler;
+    }
+}
---
+++ expansion/compatibility/Towny/src/main/java/combatlogx/expansion/compatibility/region/towny/TownyRegionHandler.java
@@ -0,0 +1,83 @@
+package combatlogx.expansion.compatibility.region.towny;
+
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+import org.bukkit.Location;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
+
+import com.github.sirblobman.api.configuration.ConfigurationManager;
+import com.github.sirblobman.combatlogx.api.expansion.region.RegionExpansion;
+import com.github.sirblobman.combatlogx.api.expansion.region.RegionHandler;
+import com.github.sirblobman.combatlogx.api.object.TagType;
+
+import com.palmergames.bukkit.towny.TownyAPI;
+import com.palmergames.bukkit.towny.object.Resident;
+import com.palmergames.bukkit.towny.object.Town;
+import com.palmergames.bukkit.towny.object.TownBlock;
+import com.palmergames.bukkit.towny.object.TownyPermission;
+import com.palmergames.bukkit.towny.object.TownyWorld;
+import com.palmergames.bukkit.towny.war.flagwar.FlagWar;
+
+public class TownyRegionHandler extends RegionHandler {
+    public TownyRegionHandler(TownyExpansion expansion) {
+        super(expansion);
+    }
+
+    @Override
+    public String getEntryDeniedMessagePath(TagType tagType) {
+        return "expansion.towny-compatibility-no-entry";
+    }
+
+    @Override
+    @SuppressWarnings("deprecation")
+    public boolean isSafeZone(Player player, Location location, TagType tagType) {
+        if(tagType != TagType.PLAYER) return false;
+
+        RegionExpansion expansion = getExpansion();
+        ConfigurationManager configurationManager = expansion.getConfigurationManager();
+        YamlConfiguration configuration = configurationManager.get("config.yml");
+
+        TownBlock townBlock = getTownBlock(location);
+        if(configuration.getBoolean("prevent-all-town-entries", false)
+                && isOwnTown(townBlock, player)) {
+            return true;
+        }
+
+        TownyAPI townyAPI = TownyAPI.getInstance();
+        if(townyAPI.isWarTime()) return false;
+
+        TownyWorld townyWorld = townBlock.getWorld();
+        if(townyWorld == null || townyWorld.isForcePVP()) return false;
+
+        Town town = townBlock.getTownOrNull();
+        if(town == null || town.isPVP() || town.isAdminEnabledPVP()) return false;
+
+        // TODO: Use FlagWar plugin when it is released instead of this deprecated method.
+        if(FlagWar.isUnderAttack(town)) return false;
+
+        TownyPermission townBlockPermissions = townBlock.getPermissions();
+        return !townBlockPermissions.pvp;
+    }
+
+    private TownBlock getTownBlock(Location location) {
+        TownyAPI townyAPI = TownyAPI.getInstance();
+        return townyAPI.getTownBlock(location);
+    }
+
+    private boolean isOwnTown(TownBlock townBlock, Player player) {
+        if(townBlock == null) return false;
+
+        Town town = townBlock.getTownOrNull();
+        if(town == null) return false;
+
+        List<Resident> residentList = town.getResidents();
+        Set<UUID> residentIdList = residentList.stream().map(Resident::getUUID).collect(Collectors.toSet());
+
+        UUID playerId = player.getUniqueId();
+        return residentIdList.contains(playerId);
+    }
+}
