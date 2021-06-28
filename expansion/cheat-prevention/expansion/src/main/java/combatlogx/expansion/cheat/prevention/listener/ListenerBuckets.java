e2b7899fdbb4bbd8b12ed6363410b68a7c6383c1
SirBlobman <blobmansrealm@gmail.com>
2021-06-28 14:59:52 -0400

    11.0.0.0: Added material options for buckets in Cheat Prevention.

--- expansion/cheat-prevention/expansion/src/main/java/combatlogx/expansion/cheat/prevention/listener/ListenerBuckets.java
+++ expansion/cheat-prevention/expansion/src/main/java/combatlogx/expansion/cheat/prevention/listener/ListenerBuckets.java
@@ -1,5 +1,8 @@
 package combatlogx.expansion.cheat.prevention.listener;
 
+import java.util.List;
+
+import org.bukkit.Material;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -8,6 +11,7 @@ import org.bukkit.event.player.PlayerBucketEmptyEvent;
 import org.bukkit.event.player.PlayerBucketFillEvent;
 
 import com.github.sirblobman.api.configuration.ConfigurationManager;
+import com.github.sirblobman.api.xseries.XMaterial;
 import com.github.sirblobman.combatlogx.api.expansion.Expansion;
 
 public final class ListenerBuckets extends CheatPreventionListener {
@@ -18,7 +22,10 @@ public final class ListenerBuckets extends CheatPreventionListener {
     @EventHandler(priority=EventPriority.NORMAL, ignoreCancelled=true)
     public void onBucketEmpty(PlayerBucketEmptyEvent e) {
         Player player = e.getPlayer();
-        if(isInCombat(player) && shouldPreventEmpty()) {
+        Material bukkitMaterial = e.getBucket();
+        XMaterial material = XMaterial.matchXMaterial(bukkitMaterial);
+
+        if(isInCombat(player) && shouldPreventEmpty(material)) {
             e.setCancelled(true);
             sendMessage(player, "expansion.cheat-prevention.buckets.no-empty", null);
         }
@@ -27,7 +34,10 @@ public final class ListenerBuckets extends CheatPreventionListener {
     @EventHandler(priority=EventPriority.NORMAL, ignoreCancelled=true)
     public void onBucketEmpty(PlayerBucketFillEvent e) {
         Player player = e.getPlayer();
-        if(isInCombat(player) && shouldPreventFill()) {
+        Material bukkitMaterial = e.getBucket();
+        XMaterial material = XMaterial.matchXMaterial(bukkitMaterial);
+
+        if(isInCombat(player) && shouldPreventFill(material)) {
             e.setCancelled(true);
             sendMessage(player, "expansion.cheat-prevention.buckets.no-fill", null);
         }
@@ -36,16 +46,24 @@ public final class ListenerBuckets extends CheatPreventionListener {
     private YamlConfiguration getConfiguration() {
         Expansion expansion = getExpansion();
         ConfigurationManager configurationManager = expansion.getConfigurationManager();
-        return configurationManager.get("items.yml");
+        return configurationManager.get("buckets.yml");
     }
 
-    private boolean shouldPreventEmpty() {
+    private boolean shouldPreventEmpty(XMaterial material) {
         YamlConfiguration configuration = getConfiguration();
-        return configuration.getBoolean("prevent-bucket-empty", false);
+        if(!configuration.getBoolean("prevent-bucket-empty", false)) return false;
+
+        String materialName = material.name();
+        List<String> preventList = configuration.getStringList("prevent-bucket-empty-list");
+        return preventList.contains(materialName);
     }
 
-    private boolean shouldPreventFill() {
+    private boolean shouldPreventFill(XMaterial material) {
         YamlConfiguration configuration = getConfiguration();
-        return configuration.getBoolean("prevent-bucket-fill", false);
+        if(!configuration.getBoolean("prevent-bucket-fill", false)) return false;
+
+        String materialName = material.name();
+        List<String> preventList = configuration.getStringList("prevent-bucket-fill-list");
+        return preventList.contains(materialName);
     }
 }
