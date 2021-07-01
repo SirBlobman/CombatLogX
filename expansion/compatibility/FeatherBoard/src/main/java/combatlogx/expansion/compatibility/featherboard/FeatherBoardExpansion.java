6b54bac7417684203f43d91761c52efb01d43442
SirBlobman <blobmansrealm@gmail.com>
2021-07-01 06:59:54 -0400

    11.0.0.0: Created FeatherBoard Compatibility expansion.

---
+++ expansion/compatibility/FeatherBoard/src/main/java/combatlogx/expansion/compatibility/featherboard/FeatherBoardExpansion.java
@@ -0,0 +1,42 @@
+package combatlogx.expansion.compatibility.featherboard;
+
+import com.github.sirblobman.api.configuration.ConfigurationManager;
+import com.github.sirblobman.combatlogx.api.ICombatLogX;
+import com.github.sirblobman.combatlogx.api.expansion.Expansion;
+import com.github.sirblobman.combatlogx.api.expansion.ExpansionManager;
+
+import combatlogx.expansion.compatibility.featherboard.listener.ListenerFeatherBoard;
+
+public final class FeatherBoardExpansion extends Expansion {
+    public FeatherBoardExpansion(ICombatLogX plugin) {
+        super(plugin);
+    }
+
+    @Override
+    public void onLoad() {
+        ConfigurationManager configurationManager = getConfigurationManager();
+        configurationManager.saveDefault("config.yml");
+    }
+
+    @Override
+    public void onEnable() {
+        if(!checkDependency("FeatherBoard", true, "5")) {
+            ICombatLogX plugin = getPlugin();
+            ExpansionManager expansionManager = plugin.getExpansionManager();
+            expansionManager.disableExpansion(this);
+            return;
+        }
+
+        new ListenerFeatherBoard(this).register();
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+    @Override
+    public void reloadConfig() {
+
+    }
+}
---
+++ expansion/compatibility/FeatherBoard/src/main/java/combatlogx/expansion/compatibility/featherboard/listener/ListenerFeatherBoard.java
@@ -0,0 +1,51 @@
+package combatlogx.expansion.compatibility.featherboard.listener;
+
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+
+import com.github.sirblobman.api.configuration.ConfigurationManager;
+import com.github.sirblobman.combatlogx.api.event.PlayerTagEvent;
+import com.github.sirblobman.combatlogx.api.event.PlayerUntagEvent;
+import com.github.sirblobman.combatlogx.api.expansion.ExpansionListener;
+
+import be.maximvdw.featherboard.api.FeatherBoardAPI;
+import combatlogx.expansion.compatibility.featherboard.FeatherBoardExpansion;
+import org.jetbrains.annotations.NotNull;
+
+public final class ListenerFeatherBoard extends ExpansionListener {
+    public ListenerFeatherBoard(FeatherBoardExpansion expansion) {
+        super(expansion);
+    }
+
+    @EventHandler(priority=EventPriority.MONITOR, ignoreCancelled=true)
+    public void onTag(PlayerTagEvent e) {
+        Player player = e.getPlayer();
+        showTrigger(player);
+    }
+
+    @EventHandler(priority=EventPriority.MONITOR, ignoreCancelled=true)
+    public void onUntag(PlayerUntagEvent e) {
+        Player player = e.getPlayer();
+        removeTrigger(player);
+    }
+
+    @NotNull
+    private String getTriggerName() {
+        ConfigurationManager configurationManager = getExpansionConfigurationManager();
+        YamlConfiguration configuration = configurationManager.get("config.yml");
+        String triggerName = configuration.getString("trigger-name");
+        return (triggerName == null || triggerName.isEmpty() ? "combatlogx" : triggerName);
+    }
+
+    private void showTrigger(Player player) {
+        String triggerName = getTriggerName();
+        FeatherBoardAPI.showScoreboard(player, triggerName, true);
+    }
+
+    private void removeTrigger(Player player) {
+        String triggerName = getTriggerName();
+        FeatherBoardAPI.removeScoreboardOverride(player, triggerName, true);
+    }
+}
