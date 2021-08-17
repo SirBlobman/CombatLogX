f8b496f3aa78749178c25a1a2f831ee0ee97f7e1
SirBlobman <blobmansrealm@gmail.com>
2021-08-17 09:00:25 -0400

    11.0.0.0: Create Konquest Expansion
    Closes #255 once tested.

---
+++ expansion/compatibility/Konquest/src/main/java/combatlogx/expansion/compatibility/region/konquest/KonquestExpansion.java
@@ -0,0 +1,28 @@
+package combatlogx.expansion.compatibility.region.konquest;
+
+import com.github.sirblobman.combatlogx.api.ICombatLogX;
+import com.github.sirblobman.combatlogx.api.expansion.region.RegionExpansion;
+import com.github.sirblobman.combatlogx.api.expansion.region.RegionHandler;
+
+public final class KonquestExpansion extends RegionExpansion {
+    private RegionHandler regionHandler;
+    
+    public KonquestExpansion(ICombatLogX plugin) {
+        super(plugin);
+        this.regionHandler = null;
+    }
+    
+    @Override
+    public boolean checkDependencies() {
+        return checkDependency("Konquest", true, "0.5");
+    }
+    
+    @Override
+    public RegionHandler getRegionHandler() {
+        if(this.regionHandler == null) {
+            this.regionHandler = new KonquestRegionHandler(this);
+        }
+    
+        return this.regionHandler;
+    }
+}
---
+++ expansion/compatibility/Konquest/src/main/java/combatlogx/expansion/compatibility/region/konquest/KonquestRegionHandler.java
@@ -0,0 +1,34 @@
+package combatlogx.expansion.compatibility.region.konquest;
+
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+
+import com.github.sirblobman.combatlogx.api.expansion.region.RegionHandler;
+import com.github.sirblobman.combatlogx.api.object.TagType;
+
+import konquest.Konquest;
+import konquest.manager.KingdomManager;
+import konquest.model.KonKingdom;
+import konquest.model.KonTerritory;
+
+public class KonquestRegionHandler extends RegionHandler {
+    public KonquestRegionHandler(KonquestExpansion expansion) {
+        super(expansion);
+    }
+    
+    @Override
+    public String getEntryDeniedMessagePath(TagType tagType) {
+        return "expansion.region-protection.konquest-no-entry";
+    }
+    
+    @Override
+    public boolean isSafeZone(Player player, Location location, TagType tagType) {
+        Konquest konquest = Konquest.getInstance();
+        KingdomManager kingdomManager = konquest.getKingdomManager();
+        KonTerritory chunkTerritory = kingdomManager.getChunkTerritory(location);
+        if(chunkTerritory == null) return false;
+    
+        KonKingdom locationKingdom = chunkTerritory.getKingdom();
+        return (locationKingdom != null);
+    }
+}
