904ab6c10c30bd7d02e05bcd19341b8efa0b3978
SirBlobman <blobmansrealm@gmail.com>
2021-08-17 09:36:03 -0400

    11.0.0.0: Add passenger to vulnerable check.
    Closes #244

--- api/src/main/java/com/github/sirblobman/combatlogx/api/expansion/region/RegionVulnerableListener.java
+++ api/src/main/java/com/github/sirblobman/combatlogx/api/expansion/region/RegionVulnerableListener.java
@@ -1,5 +1,8 @@
 package com.github.sirblobman.combatlogx.api.expansion.region;
 
+import java.util.Collections;
+import java.util.List;
+
 import org.bukkit.Location;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
@@ -8,8 +11,9 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 
-import com.github.sirblobman.combatlogx.api.manager.ICombatManager;
+import com.github.sirblobman.api.utility.VersionUtility;
 import com.github.sirblobman.combatlogx.api.expansion.ExpansionListener;
+import com.github.sirblobman.combatlogx.api.manager.ICombatManager;
 import com.github.sirblobman.combatlogx.api.object.NoEntryMode;
 import com.github.sirblobman.combatlogx.api.object.TagType;
 
@@ -28,9 +32,9 @@ public final class RegionVulnerableListener extends ExpansionListener {
         if(noEntryMode != NoEntryMode.VULNERABLE) return;
 
         Entity damaged = e.getEntity();
-        if(!(damaged instanceof Player)) return;
-
-        Player player = (Player) damaged;
+        Player player = getPlayerOrPassenger(damaged);
+        if(player == null) return;
+        
         if(isInCombat(player)) {
             ICombatManager combatManager = getCombatManager();
             LivingEntity enemy = combatManager.getEnemy(player);
@@ -47,5 +51,36 @@ public final class RegionVulnerableListener extends ExpansionListener {
         if(enemy == null) return TagType.UNKNOWN;
         if(enemy instanceof Player) return TagType.PLAYER;
         return TagType.MOB;
+    }
+    
+    private Player getPlayerOrPassenger(Entity entity) {
+        if(entity instanceof Player) {
+            return (Player) entity;
+        }
+        
+        List<Entity> passengerList = getPassengers(entity);
+        if(passengerList.isEmpty()) {
+            return null;
+        }
+        
+        for(Entity passenger : passengerList) {
+            if(passenger instanceof Player) {
+                return (Player) passenger;
+            }
+        }
+        
+        return null;
+    }
+    
+    @SuppressWarnings("deprecation")
+    private List<Entity> getPassengers(Entity entity) {
+        int minorVersion = VersionUtility.getMinorVersion();
+        if(minorVersion < 11) {
+            Entity passenger = entity.getPassenger();
+            if(passenger == null) return Collections.emptyList();
+            return Collections.singletonList(passenger);
+        }
+        
+        return entity.getPassengers();
     }
 }
