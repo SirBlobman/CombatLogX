57a7334c753902c2b7f085007a7e128c1656a3ef
SirBlobman <blobmansrealm@gmail.com>
2021-08-22 14:27:06 -0400

    11.0.0.0: Created expansion "CMI Compatibility" for vanish support.
    This was created using "CMI API 8.7.8.2". This is not guaranteed to work with CMI 9+.
    
    Closes #267

---
+++ expansion/compatibility/CMI/src/main/java/combatlogx/expansion/compatibility/cmi/CMIExpansion.java
@@ -0,0 +1,43 @@
+package combatlogx.expansion.compatibility.cmi;
+
+import com.github.sirblobman.api.configuration.ConfigurationManager;
+import com.github.sirblobman.combatlogx.api.ICombatLogX;
+import com.github.sirblobman.combatlogx.api.expansion.Expansion;
+import com.github.sirblobman.combatlogx.api.expansion.ExpansionManager;
+
+import combatlogx.expansion.compatibility.cmi.listener.ListenerCMI;
+
+public final class CMIExpansion extends Expansion {
+    public CMIExpansion(ICombatLogX plugin) {
+        super(plugin);
+    }
+    
+    @Override
+    public void onLoad() {
+        ConfigurationManager configurationManager = getConfigurationManager();
+        configurationManager.saveDefault("config.yml");
+    }
+    
+    @Override
+    public void onEnable() {
+        if(!checkDependency("CMI", true)) {
+            ICombatLogX plugin = getPlugin();
+            ExpansionManager expansionManager = plugin.getExpansionManager();
+            expansionManager.disableExpansion(this);
+            return;
+        }
+        
+        new ListenerCMI(this).register();
+    }
+    
+    @Override
+    public void onDisable() {
+        // Do Nothing
+    }
+    
+    @Override
+    public void reloadConfig() {
+        ConfigurationManager configurationManager = getConfigurationManager();
+        configurationManager.reload("config.yml");
+    }
+}
---
+++ expansion/compatibility/CMI/src/main/java/combatlogx/expansion/compatibility/cmi/listener/ListenerCMI.java
@@ -0,0 +1,60 @@
+package combatlogx.expansion.compatibility.cmi.listener;
+
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+
+import com.github.sirblobman.api.configuration.ConfigurationManager;
+import com.github.sirblobman.combatlogx.api.event.PlayerPreTagEvent;
+import com.github.sirblobman.combatlogx.api.expansion.ExpansionListener;
+
+import com.Zrips.CMI.CMI;
+import com.Zrips.CMI.Containers.CMIUser;
+import com.Zrips.CMI.PlayerManager;
+import combatlogx.expansion.compatibility.cmi.CMIExpansion;
+
+public final class ListenerCMI extends ExpansionListener {
+    public ListenerCMI(CMIExpansion expansion) {
+        super(expansion);
+    }
+
+    @EventHandler(priority=EventPriority.NORMAL, ignoreCancelled=true)
+    public void beforeCombat(PlayerPreTagEvent e) {
+        Player player = e.getPlayer();
+        if(isVanished(player) && preventVanishSelfTag()) {
+            e.setCancelled(true);
+        }
+
+        LivingEntity enemy = e.getEnemy();
+        if(enemy instanceof Player) {
+            Player other = (Player) enemy;
+            if(isVanished(other) && preventVanishOtherTag()) {
+                e.setCancelled(true);
+            }
+        }
+    }
+
+    private YamlConfiguration getConfiguration() {
+        ConfigurationManager configurationManager = getExpansionConfigurationManager();
+        return configurationManager.get("config.yml");
+    }
+
+    private boolean preventVanishSelfTag() {
+        YamlConfiguration configuration = getConfiguration();
+        return configuration.getBoolean("prevent-vanish-tagging-self");
+    }
+
+    private boolean preventVanishOtherTag() {
+        YamlConfiguration configuration = getConfiguration();
+        return configuration.getBoolean("prevent-vanish-tagging-other");
+    }
+
+    private boolean isVanished(Player player) {
+        CMI cmi = CMI.getInstance();
+        PlayerManager playerManager = cmi.getPlayerManager();
+        CMIUser cmiUser = playerManager.getUser(player);
+        return (cmiUser != null && cmiUser.isVanished());
+    }
+}
