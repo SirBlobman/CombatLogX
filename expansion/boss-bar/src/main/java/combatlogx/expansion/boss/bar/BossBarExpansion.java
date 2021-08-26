ceb2aae24668b54a0b17a46fb72ae65e971c1d33
SirBlobman <blobmansrealm@gmail.com>
2021-08-25 22:42:24 -0400

    11.0.0.0: Remove BossBarAPI check from Boss Bar expansion.
    This also reverts commit 6af115c02d681c2255976c0589f93b053dec051c.

--- expansion/boss-bar/src/main/java/combatlogx/expansion/boss/bar/BossBarExpansion.java
+++ expansion/boss-bar/src/main/java/combatlogx/expansion/boss/bar/BossBarExpansion.java
@@ -1,14 +1,10 @@
 package combatlogx.expansion.boss.bar;
 
-import java.util.logging.Logger;
-
 import com.github.sirblobman.api.configuration.ConfigurationManager;
 import com.github.sirblobman.api.nms.MultiVersionHandler;
-import com.github.sirblobman.api.utility.VersionUtility;
 import com.github.sirblobman.bossbar.BossBarHandler;
 import com.github.sirblobman.combatlogx.api.ICombatLogX;
 import com.github.sirblobman.combatlogx.api.expansion.Expansion;
-import com.github.sirblobman.combatlogx.api.expansion.ExpansionManager;
 import com.github.sirblobman.combatlogx.api.manager.ITimerManager;
 
 public final class BossBarExpansion extends Expansion {
@@ -30,16 +26,6 @@ public final class BossBarExpansion extends Expansion {
     @Override
     public void onEnable() {
         ICombatLogX plugin = getPlugin();
-        int minorVersion = VersionUtility.getMinorVersion();
-        if(minorVersion < 9 && !checkDependency("BossBarAPI", true)) {
-            Logger logger = getLogger();
-            logger.warning("The boss bar expansion requires BossBarAPI if you are in a version below 1.9!");
-
-            ExpansionManager expansionManager = plugin.getExpansionManager();
-            expansionManager.disableExpansion(this);
-            return;
-        }
-
         ITimerManager timerManager = plugin.getTimerManager();
         timerManager.addUpdaterTask(new BossBarUpdater(this));
     }
