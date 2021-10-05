5d13c75e59290cb20f252e8d5ef6e866da129fce
SirBlobman <blobmansrealm@gmail.com>
2021-10-04 23:55:58 -0400

    11.0.0.0: Moved action bar messages to language files instead of config.yml.

--- expansion/action-bar/src/main/java/combatlogx/expansion/action/bar/ActionBarExpansion.java
+++ expansion/action-bar/src/main/java/combatlogx/expansion/action/bar/ActionBarExpansion.java
@@ -5,21 +5,21 @@ import java.util.logging.Logger;
 import com.github.sirblobman.api.configuration.ConfigurationManager;
 import com.github.sirblobman.api.utility.VersionUtility;
 import com.github.sirblobman.combatlogx.api.ICombatLogX;
-import com.github.sirblobman.combatlogx.api.manager.ITimerManager;
 import com.github.sirblobman.combatlogx.api.expansion.Expansion;
 import com.github.sirblobman.combatlogx.api.expansion.ExpansionManager;
+import com.github.sirblobman.combatlogx.api.manager.ITimerManager;
 
 public final class ActionBarExpansion extends Expansion {
     public ActionBarExpansion(ICombatLogX plugin) {
         super(plugin);
     }
-
+    
     @Override
     public void onLoad() {
         ConfigurationManager configurationManager = getConfigurationManager();
         configurationManager.saveDefault("config.yml");
     }
-
+    
     @Override
     public void onEnable() {
         ICombatLogX plugin = getPlugin();
@@ -27,21 +27,21 @@ public final class ActionBarExpansion extends Expansion {
         if(minorVersion < 8) {
             Logger logger = getLogger();
             logger.warning("This expansion requires Spigot 1.8.8 or higher.");
-
+            
             ExpansionManager expansionManager = plugin.getExpansionManager();
             expansionManager.disableExpansion(this);
             return;
         }
-
+        
         ITimerManager timerManager = plugin.getTimerManager();
         timerManager.addUpdaterTask(new ActionBarUpdater(this));
     }
-
+    
     @Override
     public void onDisable() {
         // Do Nothing
     }
-
+    
     @Override
     public void reloadConfig() {
         ConfigurationManager configurationManager = getConfigurationManager();
--- expansion/action-bar/src/main/java/combatlogx/expansion/action/bar/ActionBarUpdater.java
+++ expansion/action-bar/src/main/java/combatlogx/expansion/action/bar/ActionBarUpdater.java
@@ -2,13 +2,16 @@ package combatlogx.expansion.action.bar;
 
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
+import net.md_5.bungee.api.ChatColor;
 
 import com.github.sirblobman.api.configuration.ConfigurationManager;
 import com.github.sirblobman.api.configuration.PlayerDataManager;
+import com.github.sirblobman.api.language.LanguageManager;
 import com.github.sirblobman.api.nms.MultiVersionHandler;
 import com.github.sirblobman.api.nms.PlayerHandler;
 import com.github.sirblobman.api.utility.MessageUtility;
 import com.github.sirblobman.api.utility.Validate;
+import com.github.sirblobman.api.utility.VersionUtility;
 import com.github.sirblobman.combatlogx.api.ICombatLogX;
 import com.github.sirblobman.combatlogx.api.manager.ICombatManager;
 import com.github.sirblobman.combatlogx.api.object.TimerUpdater;
@@ -16,103 +19,156 @@ import com.github.sirblobman.combatlogx.api.utility.PlaceholderHelper;
 
 public final class ActionBarUpdater implements TimerUpdater {
     private final ActionBarExpansion expansion;
-
+    
     public ActionBarUpdater(ActionBarExpansion expansion) {
         this.expansion = Validate.notNull(expansion, "expansion must not be null!");
     }
-
-    private ICombatLogX getCombatLogX() {
-        return this.expansion.getPlugin();
+    
+    private ActionBarExpansion getExpansion() {
+        return this.expansion;
     }
-
+    
+    private ICombatLogX getCombatLogX() {
+        ActionBarExpansion expansion = getExpansion();
+        return expansion.getPlugin();
+    }
+    
+    private LanguageManager getLanguageManager() {
+        ICombatLogX combatLogX = getCombatLogX();
+        return combatLogX.getLanguageManager();
+    }
+    
+    private PlayerDataManager getPlayerDataManager() {
+        ICombatLogX combatLogX = getCombatLogX();
+        return combatLogX.getPlayerDataManager();
+    }
+    
     private PlayerHandler getPlayerHandler() {
         ICombatLogX combatLogX = getCombatLogX();
         MultiVersionHandler multiVersionHandler = combatLogX.getMultiVersionHandler();
         return multiVersionHandler.getPlayerHandler();
     }
-
+    
     @Override
     public void update(Player player, long timeLeftMillis) {
-        if(isDisabled(player)) return;
-
+        if(isDisabled(player)) {
+            return;
+        }
+        
         String message = getMessage(player, timeLeftMillis);
-        if(message == null || message.isEmpty()) return;
-
+        if(message == null || message.isEmpty()) {
+            return;
+        }
+        
         PlayerHandler playerHandler = getPlayerHandler();
         playerHandler.sendActionBar(player, message);
     }
-
+    
     @Override
     public void remove(Player player) {
         update(player, 0L);
     }
-
-    private boolean isDisabled(Player player) {
-        ICombatLogX combatLogX = getCombatLogX();
-        PlayerDataManager playerDataManager = combatLogX.getPlayerDataManager();
-        YamlConfiguration playerData = playerDataManager.get(player);
-        return !playerData.getBoolean("actionbar", true);
-    }
-
-    private String getMessage(Player player, long timeLeftMillis) {
-        ConfigurationManager configurationManager = this.expansion.getConfigurationManager();
+    
+    private boolean isGlobalEnabled() {
+        ActionBarExpansion expansion = getExpansion();
+        ConfigurationManager configurationManager = expansion.getConfigurationManager();
         YamlConfiguration configuration = configurationManager.get("config.yml");
-        if(timeLeftMillis <= 0) {
-            String message = configuration.getString("ended");
-            return MessageUtility.color(message);
+        return configuration.getBoolean("enabled", true);
+    }
+    
+    private boolean isDisabled(Player player) {
+        if(isGlobalEnabled()) {
+            PlayerDataManager playerDataManager = getPlayerDataManager();
+            YamlConfiguration playerData = playerDataManager.get(player);
+            return !playerData.getBoolean("actionbar", true);
         }
-
+        
+        return true;
+    }
+    
+    private String getMessage(Player player, long timeLeftMillis) {
+        LanguageManager languageManager = getLanguageManager();
+        if(timeLeftMillis <= 0) {
+            String path = ("expansion.action-bar.ended");
+            return languageManager.getMessage(player, path, null, true);
+        }
+        
+        String path = ("expansion.action-bar.timer");
+        String message = languageManager.getMessage(player, path, null, true);
+        if(message.isEmpty()) {
+            return null;
+        }
+        
+        return replacePlaceholders(player, message, timeLeftMillis);
+    }
+    
+    private String replacePlaceholders(Player player, String message, long timeLeftMillis) {
         ICombatLogX plugin = getCombatLogX();
-        String message = configuration.getString("timer");
-        if(message == null || message.isEmpty()) return null;
-        message = MessageUtility.color(message);
-
+        
         if(message.contains("{time_left}")) {
             String timeLeftNormal = PlaceholderHelper.getTimeLeft(plugin, player);
             message = message.replace("{time_left}", timeLeftNormal);
         }
-
+        
         if(message.contains("{time_left_decimal}")) {
             String timeLeftDecimal = PlaceholderHelper.getTimeLeftDecimal(plugin, player);
             message = message.replace("{time_left_decimal}", timeLeftDecimal);
         }
-
+        
         if(message.contains("{bars}")) {
             String bars = getBars(player, timeLeftMillis);
             message = message.replace("{bars}", bars);
         }
-
+        
         return message;
     }
-
+    
     private String getBars(Player player, long timeLeftMillis) {
         ConfigurationManager configurationManager = this.expansion.getConfigurationManager();
         YamlConfiguration configuration = configurationManager.get("config.yml");
-
+        
         int scale = configuration.getInt("scale");
-        String leftColor = configuration.getString("left-color");
-        String leftSymbol = configuration.getString("left-symbol");
-        String rightColor = configuration.getString("right-color");
-        String rightSymbol = configuration.getString("right-symbol");
-        if(leftColor == null) leftColor = "&a";
-        if(leftSymbol == null) leftSymbol = "|";
-        if(rightColor == null) rightColor = "&a";
-        if(rightSymbol == null) rightSymbol = "|";
-
+        String leftColorString = configuration.getString("left-color", "GREEN");
+        String leftSymbol = configuration.getString("left-symbol", "|");
+        String rightColorString = configuration.getString("right-color", "RED");
+        String rightSymbol = configuration.getString("right-symbol", "|");
+        
+        ChatColor leftColor = getChatColor(leftColorString);
+        ChatColor rightColor = getChatColor(rightColorString);
+        
         ICombatLogX plugin = getCombatLogX();
         ICombatManager combatManager = plugin.getCombatManager();
         double timerMaxMillis = (combatManager.getMaxTimerSeconds(player) * 1_000L);
         double progressPercent = ((double) timeLeftMillis / timerMaxMillis);
         long leftBarsCount = Math.round(scale * progressPercent);
         long rightBarsCount = (scale - leftBarsCount);
-
-        StringBuilder builder = new StringBuilder(leftColor);
-        for(long i = 0; i < leftBarsCount; i++) builder.append(leftSymbol);
-
+        
+        StringBuilder builder = new StringBuilder();
+        builder.append(leftColor);
+        for(long i = 0; i < leftBarsCount; i++) {
+            builder.append(leftSymbol);
+        }
+        
         builder.append(rightColor);
-        for(long i = 0; i < rightBarsCount; i++) builder.append(rightSymbol);
-
+        for(long i = 0; i < rightBarsCount; i++) {
+            builder.append(rightSymbol);
+        }
+        
         String barsString = builder.toString();
         return MessageUtility.color(barsString);
+    }
+    
+    @SuppressWarnings("deprecation")
+    private ChatColor getChatColor(String value) {
+        try {
+            int minorVersion = VersionUtility.getMinorVersion();
+            if(minorVersion < 16) {
+                return ChatColor.valueOf(value);
+            }
+            
+            return ChatColor.of(value);
+        } catch(IllegalArgumentException ex) {
+            return ChatColor.WHITE;
+        }
     }
 }
