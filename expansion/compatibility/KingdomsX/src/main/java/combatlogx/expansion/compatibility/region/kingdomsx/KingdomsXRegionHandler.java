df2afad09598acf37d26122837bade115596d194
SirBlobman <blobmansrealm@gmail.com>
2021-10-29 06:56:08 -0400

    11.0.0.0: Update KingdomsX version to 1.11.9.

--- expansion/compatibility/KingdomsX/src/main/java/combatlogx/expansion/compatibility/region/kingdomsx/KingdomsXRegionHandler.java
+++ expansion/compatibility/KingdomsX/src/main/java/combatlogx/expansion/compatibility/region/kingdomsx/KingdomsXRegionHandler.java
@@ -1,11 +1,15 @@
 package combatlogx.expansion.compatibility.region.kingdomsx;
 
+import java.util.Set;
+import java.util.UUID;
+
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
 import com.github.sirblobman.combatlogx.api.expansion.region.RegionHandler;
 import com.github.sirblobman.combatlogx.api.object.TagType;
 
+import org.kingdoms.constants.kingdom.Kingdom;
 import org.kingdoms.constants.land.Land;
 
 public final class KingdomsXRegionHandler extends RegionHandler {
@@ -20,13 +24,22 @@ public final class KingdomsXRegionHandler extends RegionHandler {
 
     @Override
     public boolean isSafeZone(Player player, Location location, TagType tagType) {
-        if(tagType != TagType.PLAYER) return false;
-
-        Land land = Land.getLand(location);
-        if(land != null) {
-            return !land.isBeingInvaded();
+        if(tagType != TagType.PLAYER) {
+            return false;
         }
 
-        return false;
+        Land land = Land.getLand(location);
+        if(land == null || !land.isClaimed() || land.isBeingInvaded()) {
+            return false;
+        }
+    
+        Kingdom kingdom = land.getKingdom();
+        if(kingdom == null) {
+            return false;
+        }
+    
+        UUID playerId = player.getUniqueId();
+        Set<UUID> memberSet = kingdom.getMembers();
+        return !memberSet.contains(playerId);
     }
 }
