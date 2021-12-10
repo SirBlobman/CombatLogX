e6e7c836f9896f5748210b06a6f73f87c80b5b74
SirBlobman <blobmansrealm@gmail.com>
2021-12-10 08:40:27 -0500

    11.0.0.0: Add option to force disable elytra when a player is tagged.
    Closes #328

--- expansion/cheat-prevention/expansion/src/main/java/combatlogx/expansion/cheat/prevention/listener/ListenerElytra.java
+++ expansion/cheat-prevention/expansion/src/main/java/combatlogx/expansion/cheat/prevention/listener/ListenerElytra.java
@@ -18,32 +18,45 @@ public final class ListenerElytra extends CheatPreventionListener {
     
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void onTag(PlayerTagEvent e) {
-        if(isAllowed()) return;
         Player player = e.getPlayer();
-        player.setGliding(false);
-        sendMessage(player, "expansion.cheat-prevention.elytra.force-disabled", null);
+        if(player.isGliding() && shouldForcePrevent()) {
+            player.setGliding(false);
+            sendMessage(player, "expansion.cheat-prevention.elytra.force-disabled", null);
+        }
     }
     
     @EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
     public void onToggle(EntityToggleGlideEvent e) {
+        if(!e.isGliding()) {
+            return;
+        }
+        
         Entity entity = e.getEntity();
-        if(!(entity instanceof Player)) return;
+        if(!(entity instanceof Player)) {
+            return;
+        }
         
         Player player = (Player) entity;
-        if(!isInCombat(player) || !e.isGliding() || isAllowed()) return;
+        if(isAllowed() || !isInCombat(player)) {
+            return;
+        }
         
         e.setCancelled(true);
         sendMessage(player, "expansion.cheat-prevention.elytra.no-gliding", null);
     }
     
     private YamlConfiguration getConfiguration() {
-        Expansion expansion = getExpansion();
-        ConfigurationManager configurationManager = expansion.getConfigurationManager();
+        ConfigurationManager configurationManager = getExpansionConfigurationManager();
         return configurationManager.get("items.yml");
     }
     
     private boolean isAllowed() {
         YamlConfiguration configuration = getConfiguration();
         return !configuration.getBoolean("prevent-elytra");
+    }
+    
+    private boolean shouldForcePrevent() {
+        YamlConfiguration configuration = getConfiguration();
+        return configuration.getBoolean("force-prevent-elytra", false);
     }
 }
