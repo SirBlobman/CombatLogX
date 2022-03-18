d9f83e67976247932ec9f655be5ed3369451dbb7
SirBlobman <blobmansrealm@gmail.com>
2022-03-18 17:45:40 -0400

    Use the Konquest API as intended and not internal code.

--- expansion/compatibility/Konquest/src/main/java/combatlogx/expansion/compatibility/region/konquest/KonquestRegionHandler.java
+++ expansion/compatibility/Konquest/src/main/java/combatlogx/expansion/compatibility/region/konquest/KonquestRegionHandler.java
@@ -1,15 +1,19 @@
 package combatlogx.expansion.compatibility.region.konquest;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.RegisteredServiceProvider;
+import org.bukkit.plugin.ServicesManager;
 
 import com.github.sirblobman.combatlogx.api.expansion.region.RegionHandler;
 import com.github.sirblobman.combatlogx.api.object.TagType;
 
-import konquest.Konquest;
-import konquest.manager.KingdomManager;
-import konquest.model.KonKingdom;
-import konquest.model.KonTerritory;
+import konquest.api.KonquestAPI;
+import konquest.api.manager.KonquestKingdomManager;
+import konquest.api.model.KonquestKingdom;
+import konquest.api.model.KonquestTerritory;
 
 public class KonquestRegionHandler extends RegionHandler {
     public KonquestRegionHandler(KonquestExpansion expansion) {
@@ -23,14 +27,37 @@ public class KonquestRegionHandler extends RegionHandler {
     
     @Override
     public boolean isSafeZone(Player player, Location location, TagType tagType) {
-        Konquest konquest = Konquest.getInstance();
-        KingdomManager kingdomManager = konquest.getKingdomManager();
-        KonTerritory chunkTerritory = kingdomManager.getChunkTerritory(location);
-        if(chunkTerritory == null) {
+        KonquestAPI api = getAPI();
+        if(api == null) {
             return false;
         }
         
-        KonKingdom locationKingdom = chunkTerritory.getKingdom();
-        return (locationKingdom != null);
+        KonquestKingdomManager kingdomManager = api.getKingdomManager();
+        if(kingdomManager == null) {
+            return false;
+        }
+        
+        KonquestTerritory territory = kingdomManager.getChunkTerritory(location);
+        if(territory == null) {
+            return false;
+        }
+    
+        KonquestKingdom kingdom = territory.getKingdom();
+        return (kingdom != null);
+    }
+    
+    private KonquestAPI getAPI() {
+        PluginManager pluginManager = Bukkit.getPluginManager();
+        if(!pluginManager.isPluginEnabled("Konquest")) {
+            return null;
+        }
+    
+        ServicesManager servicesManager = Bukkit.getServicesManager();
+        RegisteredServiceProvider<KonquestAPI> registration = servicesManager.getRegistration(KonquestAPI.class);
+        if(registration == null) {
+            return null;
+        }
+        
+        return registration.getProvider();
     }
 }
