fb632851f9b2868a9610cc085225b5e9bdcdf13d
SirBlobman <blobmansrealm@gmail.com>
2022-05-05 11:27:11 -0400

    11.0.0.0: Ensure that the glowing effect is removed when players leave the game or die.

--- expansion/glowing/src/main/java/combatlogx/expansion/glowing/listener/ListenerGlow.java
+++ expansion/glowing/src/main/java/combatlogx/expansion/glowing/listener/ListenerGlow.java
@@ -3,6 +3,9 @@ package combatlogx.expansion.glowing.listener;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerKickEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 import com.github.sirblobman.combatlogx.api.event.PlayerTagEvent;
 import com.github.sirblobman.combatlogx.api.event.PlayerUntagEvent;
@@ -23,6 +26,24 @@ public final class ListenerGlow extends ExpansionListener {
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void onUntag(PlayerUntagEvent e) {
         Player player = e.getPlayer();
+        player.setGlowing(false);
+    }
+    
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    public void onQuit(PlayerQuitEvent e) {
+        Player player = e.getPlayer();
+        player.setGlowing(false);
+    }
+    
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    public void onKick(PlayerKickEvent e) {
+        Player player = e.getPlayer();
+        player.setGlowing(false);
+    }
+    
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    public void onDeath(PlayerDeathEvent e) {
+        Player player = e.getEntity();
         player.setGlowing(false);
     }
 }
