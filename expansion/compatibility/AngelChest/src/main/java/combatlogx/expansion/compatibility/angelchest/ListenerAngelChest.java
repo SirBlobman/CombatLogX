99d200386b127b33d185a196425444d66f67e7d4
SirBlobman <blobmansrealm@gmail.com>
2022-07-17 11:58:48 -0400

    11.0.0.0: Code style changes for AngelChest expansion.

--- expansion/compatibility/AngelChest/src/main/java/combatlogx/expansion/compatibility/angelchest/ListenerAngelChest.java
+++ expansion/compatibility/AngelChest/src/main/java/combatlogx/expansion/compatibility/angelchest/ListenerAngelChest.java
@@ -2,53 +2,77 @@ package combatlogx.expansion.compatibility.angelchest;
 
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 
 import com.github.sirblobman.api.configuration.ConfigurationManager;
 import com.github.sirblobman.combatlogx.api.expansion.Expansion;
 import com.github.sirblobman.combatlogx.api.expansion.ExpansionListener;
 
 import de.jeff_media.angelchest.events.AngelChestOpenEvent;
+import de.jeff_media.angelchest.events.AngelChestOpenEvent.Reason;
 
 public final class ListenerAngelChest extends ExpansionListener {
     public ListenerAngelChest(final Expansion expansion) {
         super(expansion);
     }
 
-    @EventHandler
-    public void onAngelChestOpen(AngelChestOpenEvent event) {
-        Player player = event.getPlayer();
+    @EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
+    public void onAngelChestOpen(AngelChestOpenEvent e) {
+        Player player = e.getPlayer();
+        if (!isInCombat(player)) {
+            return;
+        }
 
-        if (!isInCombat(player)) return;
-
-        ConfigurationManager configurationManager = getExpansionConfigurationManager();
-        YamlConfiguration configuration = configurationManager.get("config.yml");
-
-        switch (event.getReason()) {
-            case BREAK: {
-                if (configuration.getBoolean("prevent-breaking", true)) {
-                    sendMessageWithPrefix(player, "expansion.angel-chest.prevent-breaking", null, true);
-                    event.setCancelled(true);
-                }
+        Reason reason = e.getReason();
+        switch (reason) {
+            case BREAK:
+                checkBreaking(player, e);
                 break;
-            }
-
-            case OPEN_GUI: {
-                if (configuration.getBoolean("prevent-opening", true)) {
-                    sendMessageWithPrefix(player, "expansion.angel-chest.prevent-opening", null, true);
-                    event.setCancelled(true);
-                }
+            case OPEN_GUI:
+                checkOpening(player, e);
                 break;
-            }
-
-            case FAST_LOOT: {
-                if (configuration.getBoolean("prevent-fast-looting", true)) {
-                    sendMessageWithPrefix(player, "expansion.angel-chest.prevent-fast-looting", null, true);
-                    event.setCancelled(true);
-                }
+            case FAST_LOOT:
+                checkFastLooting(player, e);
                 break;
-            }
+            default:
+                break;
         }
     }
 
+    private YamlConfiguration getConfiguration() {
+        ConfigurationManager configurationManager = getExpansionConfigurationManager();
+        return configurationManager.get("config.yml");
+    }
+
+    private void checkBreaking(Player player, Cancellable e) {
+        YamlConfiguration configuration = getConfiguration();
+        if (!configuration.getBoolean("prevent-breaking", true)) {
+            return;
+        }
+
+        sendMessageWithPrefix(player, "expansion.angel-chest.prevent-breaking", null, true);
+        e.setCancelled(true);
+    }
+
+    private void checkOpening(Player player, Cancellable e) {
+        YamlConfiguration configuration = getConfiguration();
+        if (!configuration.getBoolean("prevent-opening", true)) {
+            return;
+        }
+
+        sendMessageWithPrefix(player, "expansion.angel-chest.prevent-opening", null, true);
+        e.setCancelled(true);
+    }
+
+    private void checkFastLooting(Player player, Cancellable e) {
+        YamlConfiguration configuration = getConfiguration();
+        if (!configuration.getBoolean("prevent-fast-looting", true)) {
+            return;
+        }
+
+        sendMessageWithPrefix(player, "expansion.angel-chest.prevent-fast-looting", null, true);
+        e.setCancelled(true);
+    }
 }
