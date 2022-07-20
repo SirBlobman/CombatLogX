0d0fad2b4a2bf77677c1ca81dc17a4df94645e97
SirBlobman <blobmansrealm@gmail.com>
2022-07-20 00:34:13 -0400

    11.0.0.0: Fix MarriageMaster expansion not checking the teleport event properly.

--- expansion/compatibility/MarriageMaster/src/main/java/combatlogx/expansion/compatibility/marriagemaster/listener/ListenerMarriageMaster.java
+++ expansion/compatibility/MarriageMaster/src/main/java/combatlogx/expansion/compatibility/marriagemaster/listener/ListenerMarriageMaster.java
@@ -8,6 +8,7 @@ import com.github.sirblobman.combatlogx.api.expansion.Expansion;
 import com.github.sirblobman.combatlogx.api.expansion.ExpansionListener;
 
 import at.pcgamingfreaks.MarriageMaster.Bukkit.API.Events.TPEvent;
+import at.pcgamingfreaks.MarriageMaster.Bukkit.API.Marriage;
 import at.pcgamingfreaks.MarriageMaster.Bukkit.API.MarriagePlayer;
 
 public final class ListenerMarriageMaster extends ExpansionListener {
@@ -17,16 +18,28 @@ public final class ListenerMarriageMaster extends ExpansionListener {
 
     @EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
     public void onTeleport(TPEvent e) {
-        MarriagePlayer marriagePlayer = e.getPlayer();
-        Player bukkitPlayer = marriagePlayer.getPlayerOnline();
-        if (bukkitPlayer == null) {
+        MarriagePlayer teleporter = e.getPlayer();
+        Player bukkitTeleporter = teleporter.getPlayerOnline();
+        if(bukkitTeleporter == null) {
             return;
         }
 
-        if (isInCombat(bukkitPlayer)) {
+        Marriage marriageData = e.getMarriageData();
+        MarriagePlayer partner = marriageData.getPartner(teleporter);
+        if(partner != null) {
+            Player bukkitPartner = partner.getPlayerOnline();
+            if(bukkitPartner != null && isInCombat(bukkitPartner)) {
+                e.setCancelled(true);
+                String messagePath = ("expansion.marriagemaster-compatibility.prevent-teleport-partner");
+                sendMessageWithPrefix(bukkitTeleporter, messagePath, null, true);
+                return;
+            }
+        }
+
+        if(isInCombat(bukkitTeleporter)) {
             e.setCancelled(true);
-            String messagePath = ("expansion.marriagemaster-compatibility.prevent-teleport");
-            sendMessageWithPrefix(bukkitPlayer, messagePath, null, true);
+            String messagePath = ("expansion.marriagemaster-compatibility.prevent-teleport-self");
+            sendMessageWithPrefix(bukkitTeleporter, messagePath, null, true);
         }
     }
 }
