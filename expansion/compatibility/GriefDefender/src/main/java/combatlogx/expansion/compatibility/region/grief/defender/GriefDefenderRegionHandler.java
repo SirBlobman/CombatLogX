fbdc58096a72059e6ffc183a2fd21e427e9d07ac
bloodshot <jdroque@gmail.com>
2022-08-20 10:01:19 -0400

    Improve GriefDefender region handler.

--- expansion/compatibility/GriefDefender/src/main/java/combatlogx/expansion/compatibility/region/grief/defender/GriefDefenderRegionHandler.java
+++ expansion/compatibility/GriefDefender/src/main/java/combatlogx/expansion/compatibility/region/grief/defender/GriefDefenderRegionHandler.java
@@ -1,7 +1,5 @@
 package combatlogx.expansion.compatibility.region.grief.defender;
 
-import java.util.HashSet;
-import java.util.Set;
 import java.util.UUID;
 
 import org.bukkit.Location;
@@ -12,15 +10,9 @@ import com.github.sirblobman.combatlogx.api.expansion.region.RegionHandler;
 import com.github.sirblobman.combatlogx.api.object.TagInformation;
 import com.github.sirblobman.combatlogx.api.object.TagType;
 
-import com.griefdefender.api.Core;
 import com.griefdefender.api.GriefDefender;
-import com.griefdefender.api.Tristate;
 import com.griefdefender.api.User;
 import com.griefdefender.api.claim.Claim;
-import com.griefdefender.api.claim.ClaimManager;
-import com.griefdefender.api.permission.Context;
-import com.griefdefender.api.permission.option.Options;
-import com.griefdefender.lib.geantyref.TypeToken;
 
 public final class GriefDefenderRegionHandler extends RegionHandler {
     public GriefDefenderRegionHandler(GriefDefenderExpansion expansion) {
@@ -34,44 +26,31 @@ public final class GriefDefenderRegionHandler extends RegionHandler {
 
     @Override
     public boolean isSafeZone(Player player, Location location, TagInformation tagInformation) {
-        TagType tagType = tagInformation.getCurrentTagType();
+        final TagType tagType = tagInformation.getCurrentTagType();
         if (tagType != TagType.PLAYER) {
             return false;
         }
 
-        Claim claim = getClaimAt(location);
+        final Claim claim = getClaimAt(location);
         if (claim == null) {
             return false;
         }
 
-        UUID playerId = player.getUniqueId();
-        Core core = GriefDefender.getCore();
-        User user = core.getUser(playerId);
+        final UUID playerId = player.getUniqueId();
+        final User user = GriefDefender.getCore().getUser(playerId);
+        if (user == null) {
+            return false;
+        }
 
-        Set<Context> contexts = new HashSet<>();
-        TypeToken<Tristate> typeTokenTristate = TypeToken.get(Tristate.class);
-        Tristate activeOptionValue = claim.getActiveOptionValue(typeTokenTristate, Options.PVP, user, contexts);
-
-        return (activeOptionValue != Tristate.TRUE);
+        return !user.canPvp(claim);
     }
 
     private Claim getClaimAt(Location location) {
-        World world = location.getWorld();
+        final World world = location.getWorld();
         if (world == null) {
             return null;
         }
 
-        UUID worldId = world.getUID();
-        int x = location.getBlockX();
-        int y = location.getBlockY();
-        int z = location.getBlockZ();
-
-        Core core = GriefDefender.getCore();
-        ClaimManager claimManager = core.getClaimManager(worldId);
-        if (claimManager == null) {
-            return null;
-        }
-
-        return claimManager.getClaimAt(x, y, z);
+        return GriefDefender.getCore().getClaimAt(location);
     }
 }
