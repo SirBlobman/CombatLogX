db2b0b2459d923bb6513c2c26b5936d889764c7b
SirBlobman <blobmansrealm@gmail.com>
2022-08-23 07:51:57 -0400

    11.1.0.4: Fix code style in GriefDefender expansion and bump version.

--- expansion/compatibility/GriefDefender/src/main/java/combatlogx/expansion/compatibility/region/grief/defender/GriefDefenderRegionHandler.java
+++ expansion/compatibility/GriefDefender/src/main/java/combatlogx/expansion/compatibility/region/grief/defender/GriefDefenderRegionHandler.java
@@ -10,9 +10,12 @@ import com.github.sirblobman.combatlogx.api.expansion.region.RegionHandler;
 import com.github.sirblobman.combatlogx.api.object.TagInformation;
 import com.github.sirblobman.combatlogx.api.object.TagType;
 
+import com.griefdefender.api.Core;
 import com.griefdefender.api.GriefDefender;
 import com.griefdefender.api.User;
 import com.griefdefender.api.claim.Claim;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public final class GriefDefenderRegionHandler extends RegionHandler {
     public GriefDefenderRegionHandler(GriefDefenderExpansion expansion) {
@@ -26,18 +29,17 @@ public final class GriefDefenderRegionHandler extends RegionHandler {
 
     @Override
     public boolean isSafeZone(Player player, Location location, TagInformation tagInformation) {
-        final TagType tagType = tagInformation.getCurrentTagType();
+        TagType tagType = tagInformation.getCurrentTagType();
         if (tagType != TagType.PLAYER) {
             return false;
         }
 
-        final Claim claim = getClaimAt(location);
+        Claim claim = getClaimAt(location);
         if (claim == null) {
             return false;
         }
 
-        final UUID playerId = player.getUniqueId();
-        final User user = GriefDefender.getCore().getUser(playerId);
+        User user = getUser(player);
         if (user == null) {
             return false;
         }
@@ -45,12 +47,26 @@ public final class GriefDefenderRegionHandler extends RegionHandler {
         return !user.canPvp(claim);
     }
 
+    @NotNull
+    private Core getCore() {
+        return GriefDefender.getCore();
+    }
+
+    @Nullable
     private Claim getClaimAt(Location location) {
-        final World world = location.getWorld();
+        World world = location.getWorld();
         if (world == null) {
             return null;
         }
 
-        return GriefDefender.getCore().getClaimAt(location);
+        Core griefDefenderCore = getCore();
+        return griefDefenderCore.getClaimAt(location);
+    }
+
+    @Nullable
+    private User getUser(Player player) {
+        Core griefDefenderCore = getCore();
+        UUID playerId = player.getUniqueId();
+        return griefDefenderCore.getUser(playerId);
     }
 }
