fb60491fc64ce70fcfb142a9fbc47c66475a42d6
SirBlobman <blobmansrealm@gmail.com>
2022-09-04 21:24:12 -0400

    Use correct decimal method in command `/ct`.

--- plugin/src/main/java/com/github/sirblobman/combatlogx/command/CommandCombatTimer.java
+++ plugin/src/main/java/com/github/sirblobman/combatlogx/command/CommandCombatTimer.java
@@ -1,13 +1,11 @@
 package com.github.sirblobman.combatlogx.command;
 
-import java.text.DecimalFormat;
 import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 
 import org.bukkit.entity.Player;
 
-import com.github.sirblobman.api.language.Language;
 import com.github.sirblobman.api.language.LanguageManager;
 import com.github.sirblobman.api.language.Replacer;
 import com.github.sirblobman.api.language.SimpleReplacer;
@@ -53,12 +51,6 @@ public final class CommandCombatTimer extends CombatLogPlayerCommand {
         ICombatManager combatManager = plugin.getCombatManager();
         LanguageManager languageManager = getLanguageManager();
 
-        Language language = languageManager.getLanguage(player);
-        if (language == null) {
-            getLogger().warning("Null language for player '" + player.getName() + "'.");
-            return;
-        }
-
         TagInformation tagInformation = combatManager.getTagInformation(player);
         if (tagInformation == null || tagInformation.isExpired()) {
             sendMessageWithPrefix(player, "error.self-not-in-combat", null);
@@ -68,8 +60,7 @@ public final class CommandCombatTimer extends CombatLogPlayerCommand {
         double timeLeftMillis = tagInformation.getMillisLeftCombined();
         double timeLeftSeconds = (timeLeftMillis / 1_000.0D);
 
-        DecimalFormat decimalFormat = language.getDecimalFormat();
-        String timeLeftString = decimalFormat.format(timeLeftSeconds);
+        String timeLeftString = languageManager.formatDecimal(player, timeLeftSeconds);
         Replacer replacer = new SimpleReplacer("{time_left}", timeLeftString);
         sendMessageWithPrefix(player, "command.combat-timer.time-left-self", replacer);
     }
@@ -78,12 +69,6 @@ public final class CommandCombatTimer extends CombatLogPlayerCommand {
         ICombatLogX plugin = getCombatLogX();
         ICombatManager combatManager = plugin.getCombatManager();
         LanguageManager languageManager = getLanguageManager();
-
-        Language language = languageManager.getLanguage(player);
-        if (language == null) {
-            getLogger().warning("Null language for player '" + player.getName() + "'.");
-            return;
-        }
 
         TagInformation tagInformation = combatManager.getTagInformation(target);
         if (tagInformation == null || tagInformation.isExpired()) {
@@ -94,8 +79,7 @@ public final class CommandCombatTimer extends CombatLogPlayerCommand {
         double timeLeftMillis = tagInformation.getMillisLeftCombined();
         double timeLeftSeconds = (timeLeftMillis / 1_000.0D);
 
-        DecimalFormat decimalFormat = language.getDecimalFormat();
-        String timeLeftString = decimalFormat.format(timeLeftSeconds);
+        String timeLeftString = languageManager.formatDecimal(player, timeLeftSeconds);
         Replacer replacer = new SimpleReplacer("{time_left}", timeLeftString);
         sendMessageWithPrefix(player, "command.combat-timer.time-left-other", replacer);
     }
