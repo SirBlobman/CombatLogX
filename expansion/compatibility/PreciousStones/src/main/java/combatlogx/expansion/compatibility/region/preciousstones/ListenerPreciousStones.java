4406ca566e5e087a1b8f299b5e6ef021f01118c8
SirBlobman <blobmansrealm@gmail.com>
2023-02-10 22:25:59 -0500

    11.2.0.0: Fix PreciousStones Compatibility expansion.

--- expansion/compatibility/PreciousStones/src/main/java/combatlogx/expansion/compatibility/region/preciousstones/ListenerPreciousStones.java
+++ expansion/compatibility/PreciousStones/src/main/java/combatlogx/expansion/compatibility/region/preciousstones/ListenerPreciousStones.java
@@ -6,6 +6,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 
 import com.github.sirblobman.api.configuration.ConfigurationManager;
+import com.github.sirblobman.api.language.LanguageManager;
 import com.github.sirblobman.combatlogx.api.expansion.ExpansionListener;
 
 import net.sacredlabyrinth.Phaed.PreciousStones.api.events.FieldPreCreationEvent;
@@ -30,6 +31,7 @@ public final class ListenerPreciousStones extends ExpansionListener {
 
         e.setCancelled(true);
         String path = ("expansion.region-protection.preciousstones.prevent-field-creation");
-        sendMessageWithPrefix(player, path, null);
+        LanguageManager languageManager = getLanguageManager();
+        languageManager.sendMessageWithPrefix(player, path);
     }
 }
