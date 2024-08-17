6b84f504e2cdd15e6d4205925b83339f020e7da1
SirBlobman <blobmansrealm@gmail.com>
2024-08-17 11:49:02 -0400

    Fix build script error.

--- expansion/cheat-prevention/modern/build.gradle.kts
+++ expansion/cheat-prevention/modern/build.gradle.kts
@@ -10,4 +10,5 @@ dependencies {
 java {
     sourceCompatibility = JavaVersion.VERSION_21
     targetCompatibility = JavaVersion.VERSION_21
+    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
 }
