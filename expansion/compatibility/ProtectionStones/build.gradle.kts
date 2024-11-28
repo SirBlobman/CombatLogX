6927ba4bcd06a7df7f6cf580ebaebda7a9d96d62
SirBlobman <blobmansrealm@gmail.com>
2024-11-28 09:55:54 -0500

    Update ProtectionStones to Java 21 due to WorldGuard requirements.

--- expansion/compatibility/ProtectionStones/build.gradle.kts
+++ expansion/compatibility/ProtectionStones/build.gradle.kts
@@ -1,6 +1,6 @@
 java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
 }
 
 repositories {
