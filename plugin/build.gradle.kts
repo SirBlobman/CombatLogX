98187d6f7f7034592e5e790179eb6eeb0fbc4da3
SirBlobman <blobmansrealm@gmail.com>
2024-11-30 15:08:50 -0500

    Update shadow jar plugin to GradleUp version.

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -1,7 +1,7 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
 plugins {
-    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("com.gradleup.shadow") version "8.3.5"
     id("maven-publish")
 }
 
