c77e29edede371629c4ed21f2db9b03c6357f5d8
SirBlobman <blobmansrealm@gmail.com>
2025-05-04 14:14:39 -0400

    11.6: Update ProtectionStones to 2.10.5

--- expansion/compatibility/ProtectionStones/build.gradle.kts
+++ expansion/compatibility/ProtectionStones/build.gradle.kts
@@ -1,13 +1,8 @@
-java {
-    sourceCompatibility = JavaVersion.VERSION_21
-    targetCompatibility = JavaVersion.VERSION_21
-}
-
 repositories {
     maven("https://maven.enginehub.org/repo/")
 }
 
 dependencies {
-    compileOnly("dev.espi:protectionstones:2.10.2")
+    compileOnly("dev.espi:protectionstones:2.10.5")
     compileOnly("com.sk89q.worldguard:worldguard-core:7.1.0-SNAPSHOT")
 }
