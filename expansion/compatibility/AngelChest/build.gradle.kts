3e82829464668c37e55da89c34416db01b9b20d6
SirBlobman <blobmansrealm@gmail.com>
2025-05-04 15:06:44 -0400

    11.6: Update AngelChest API to 13.6.1

--- expansion/compatibility/AngelChest/build.gradle.kts
+++ expansion/compatibility/AngelChest/build.gradle.kts
@@ -1,3 +1,7 @@
+repositories {
+    maven("https://repo.jeff-media.com/public/")
+}
+
 dependencies {
-    compileOnly("de.jeff_media:AngelChestAPI:9.0.0-SNAPSHOT")
+    compileOnly("de.jeff_media:AngelChestAPI:13.6.1")
 }
