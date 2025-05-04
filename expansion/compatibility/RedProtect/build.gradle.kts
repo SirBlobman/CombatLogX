d1e3e04b0f44d0b55b0444d54ce6b11721fbdf59
SirBlobman <blobmansrealm@gmail.com>
2025-05-04 14:06:08 -0400

    11.6: Update RedProtect-Spigot to 8.1.2

--- expansion/compatibility/RedProtect/build.gradle.kts
+++ expansion/compatibility/RedProtect/build.gradle.kts
@@ -1,13 +1,3 @@
-repositories {
-    maven("https://raw.githubusercontent.com/FabioZumbi12/RedProtect/mvn-repo/")
-}
-
 dependencies {
-    compileOnly("br.net.fabiozumbi12.RedProtect:RedProtect-Core:7.7.3") {
-        exclude("*", "*")
-    }
-
-    compileOnly("br.net.fabiozumbi12.RedProtect:RedProtect-Spigot:7.7.3") {
-        exclude("*", "*")
-    }
+    compileOnly("br.net.fabiozumbi12.RedProtect:RedProtect-Spigot:8.1.2")
 }
