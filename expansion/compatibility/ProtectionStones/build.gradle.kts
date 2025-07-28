244e84f603e76854465b29ecd7269a3c0c7230ea
SirBlobman <blobmansrealm@gmail.com>
2025-07-27 22:03:11 -0400

    Update ProtectionStones dependency since the EngineHub repository is temporarily down.

--- expansion/compatibility/ProtectionStones/build.gradle.kts
+++ expansion/compatibility/ProtectionStones/build.gradle.kts
@@ -1,8 +1,4 @@
-repositories {
-    maven("https://maven.enginehub.org/repo/")
-}
-
 dependencies {
     compileOnly("dev.espi:protectionstones:2.10.5")
-    compileOnly("com.sk89q.worldguard:worldguard-core:7.1.0-SNAPSHOT")
+    compileOnly("com.sk89q.worldguard:worldguard-bukkit:7.0.14")
 }
